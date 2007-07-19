179993902e77a08ce630e0a9bae6cdd6e8fcac81
Leo Moisio <semi@springlobby.info>
2007-07-19 16:51:41 +0000

    r310@multic0rn:  semi | 2007-07-19 19:51:30 +0300  yet another small simplification of the build system
    git-svn-id: http://svn.springlobby.info/trunk@307 1d9fd447-6eb1-4854-8194-394dbce8a230

--- autogen.sh
+++ autogen.sh
@@ -1,29 +1,23 @@
 #!/usr/bin/env sh
 #
-# BE CAREFUL with trees that are not completely automake-generated,
-# this script deletes all Makefile.in files it can find.
-#
 # Requires: automake 1.9, autoconf 2.57+
 # Conflicts: autoconf 2.13
 set -e
 
-# Refresh GNU autotools toolchain.
 echo Cleaning autotools files...
 find -type d -name autom4te.cache -print0 | xargs -0 rm -rf \;
 find -type f \( -name missing -o -name install-sh -o -name mkinstalldirs \
-	-o -name depcomp -o -name ltmain.sh -o -name configure \
-	-o -name config.sub -o -name config.guess \
-	-o -name Makefile.in \) -print0 | xargs -0 rm -f
+  -o -name depcomp -o -name ltmain.sh -o -name configure -o -name config.sub \
+  -o -name config.guess -o -name Makefile.in -o -name config.h.in \
+  -o -name config.h.in~ -o -name aclocal.m4 \) -print0 | xargs -0 rm -f
 
 echo Running autoreconf...
 autoreconf --force --install
 
-# For the Debian package build
 test -d debian && {
-	# link these in Debian builds
-	rm -f config.sub config.guess
-	ln -s /usr/share/misc/config.sub .
-	ln -s /usr/share/misc/config.guess .
+  rm -f config.sub config.guess
+  ln -s /usr/share/misc/config.sub .
+  ln -s /usr/share/misc/config.guess .
 }
 
 exit 0
