6c203b10495b359e05c685a2519640af6a46758e
Leo Moisio <semi@springlobby.info>
2007-07-22 22:05:24 +0000

    r346@multic0rn:  semi | 2007-07-23 01:05:17 +0300  set HAVE_WX28 in config.h when we have it
    git-svn-id: http://svn.springlobby.info/trunk@332 1d9fd447-6eb1-4854-8194-394dbce8a230

--- autogen.sh
+++ autogen.sh
@@ -3,6 +3,7 @@
 # Requires: automake 1.9, autoconf 2.57+
 # Conflicts: autoconf 2.13
 set -e
+cd $(dirname $0)
 
 echo Cleaning autotools files...
 find -type d -name autom4te.cache -print0 | xargs -0 rm -rf \;
