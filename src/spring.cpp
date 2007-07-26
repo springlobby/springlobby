faccad175f29848e30dd0982b37a230da46c714f
Leo Moisio <semi@springlobby.info>
2007-07-26 15:46:16 +0000

    fixes Ticket #13 where generated script fails when decimal point is something else because of locale
    git-svn-id: http://svn.springlobby.info/trunk@350 1d9fd447-6eb1-4854-8194-394dbce8a230

--- src/spring.cpp
+++ src/spring.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <wx/file.h>
+#include <wx/intl.h>
 
 #include "spring.h"
 #include "springprocess.h"
@@ -203,10 +204,12 @@ wxString Spring::GetScriptTxt( Battle& battle )
 
    if ( (battle.GetStartRect(AllyRevConv[i]) != 0) && (bo.starttype == ST_Choose) ) {
       BattleStartRect* sr = (BattleStartRect*)battle.GetStartRect(AllyRevConv[i]);
+      WXDLLIMPEXP_BASE wxLocale* locale = new wxLocale(wxLANGUAGE_ENGLISH, 0); // decimal point = .
       s += wxString::Format( _T("\t\tStartRectLeft=%.3f;\n"), sr->left / 200.0 );
       s += wxString::Format( _T("\t\tStartRectTop=%.3f;\n"), sr->top / 200.0 );
       s += wxString::Format( _T("\t\tStartRectRight=%.3f;\n"), sr->right / 200.0 );
       s += wxString::Format( _T("\t\tStartRectBottom=%.3f;\n"), sr->bottom / 200.0 );
+      delete locale; // automatically restores previous locale
     }
 
     s +=  _T("\t}\n");
