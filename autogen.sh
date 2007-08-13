069f9f12c032f7e2be8a039660d76818521d2d02
Leo Moisio <semi@springlobby.info>
2007-08-13 23:31:24 +0000

    added a clear error message helping you when autogen.sh fails
    git-svn-id: http://svn.springlobby.info/trunk@505 1d9fd447-6eb1-4854-8194-394dbce8a230

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 #
-# Requires: automake 1.9, autoconf 2.57+
+# Requires: automake 1.10, autoconf 2.59c+
 # Conflicts: autoconf 2.13
 set -e
 cd $(dirname $0)
@@ -13,7 +13,15 @@ find -type f \( -name missing -o -name install-sh -o -name mkinstalldirs \
   -o -name config.h.in~ -o -name aclocal.m4 \) -print0 | xargs -0 rm -f
 
 echo Running autoreconf...
-autoreconf --force --install
+autoreconf --force --install || echo \
+"Autoreconf failed. You are probably missing the required version of automake
+and/or autoconf. If you don't have the versions mentioned on the install
+instructions, upgrade them. If that is not possible, you might have luck by
+editing configure.ac and Makefile.am, but then you are on your own. If you are
+not planning to make patches or maintain packaging for some operating system,
+it is recommended to use prebuilt/prepackaged version if available for your
+operating system, or tarball source release if not, as the tarball does not
+need autogen.sh invocation."
 
 test -d debian && {
   rm -f config.sub config.guess
