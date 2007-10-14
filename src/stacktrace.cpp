46fb2f4acc4920b5aa80360ae8442a8e66ad7b6d
Andrea Piras <braindamage@springlobby.info>
2007-10-14 18:14:03 +0200

    better visual appeal of stacktrace

--- src/stacktrace.cpp
+++ src/stacktrace.cpp
@@ -15,41 +15,38 @@ StackTrace& stacktrace() { static StackTrace trace; return trace; };
 
 void StackTrace::OnStackFrame ( const wxStackFrame& frame )
 {
-  StackTraceString += wxString::Format( _T("(%d) "), frame.GetLevel() ); // (#frame_level_number)
+  StackTraceString += wxString::Format( _T("(%d) "), frame.GetLevel() ); // (frame_level_number)
   PartToHash += wxString::Format( _T("(%d) "), frame.GetLevel() );
-  StackTraceString += wxString::Format( _T("%p "), frame.GetAddress() ); // calling_add
+  StackTraceString += wxString::Format( _T("[%p] "), frame.GetAddress() ); // [calling_address]
 
   if ( frame.HasSourceLocation() )
   {
-    StackTraceString += _T("-> File: ") + frame.GetFileName() + wxString::Format( _T(" @Line : %d - "), frame.GetLine() ); // File: file_name @Line: line_number
-    PartToHash += _T("-> File: ") + frame.GetFileName() + wxString::Format( _T(" @Line : %d - "), frame.GetLine() );
-
-    StackTraceString += _T("Function: ") +  frame.GetName(); // Function: function_name
-    PartToHash += _T("Function: ") +  frame.GetName() + _T("\n");
+    StackTraceString += frame.GetName();  // function_name
+    PartToHash += frame.GetName() + _T("\n");
 
     int paramcount = frame.GetParamCount();
     if ( paramcount == 0 ){
       StackTraceString += _T("\n");
-      return;
-    }
-
-    StackTraceString += _T("(");
-    while ( paramcount > 0 )
-    {
-      wxString type;
-      wxString name;
-      wxString value;
-
-      if ( frame.GetParam( paramcount, &type, &name, &value) ) // data_type var_name = value,
+    } else {
+      StackTraceString += _T("(");
+      while ( paramcount > 0 )
       {
-        StackTraceString += _T(" ") + type;
-        StackTraceString += _T(" ") + name;
-        StackTraceString += _T(" = ") + value;
-      }
+        wxString type;
+        wxString name;
+        wxString value;
 
-      paramcount--;
-      if ( paramcount > 0 ) StackTraceString += _T(",");
+        if ( frame.GetParam( paramcount, &type, &name, &value) ) // data_type var_name = value,
+        {
+          StackTraceString += _T(" ") + type;
+          StackTraceString += _T(" ") + name;
+          StackTraceString += _T(" = ") + value;
+        }
+
+        paramcount--;
+        if ( paramcount > 0 ) StackTraceString += _T(",");
+      }
     }
+    StackTraceString += _T("->") + frame.GetFileName() + wxString::Format( _T("@:%d"), frame.GetLine() ); // File: file_name @Line: line_number
 
   }
   StackTraceString += _T("\n");
