f164ec892d6068b30414366325a1d0f9aaef76f4
Andrea Piras <braindamage@springlobby.info>
2007-11-16 13:19:06 +0100

    converted all logging functions to wxLog

--- src/utils.h
+++ src/utils.h
@@ -10,15 +10,15 @@
 //! Converts an std::string to a wxString
 #define WX_STRING(v) wxString(v.c_str(),wxConvUTF8)
 
-#define ASSERT_LOGIC(cond,msg) if(!(cond)){wxLogFatalError(_T("logic ")+ wxString(msg, wxConvUTF8 ));throw std::logic_error(msg);}
-#define ASSERT_RUNTIME(cond,msg) if(!(cond)){wxLogDebug(_T("runtime ")+ wxString(msg, wxConvUTF8 ));throw std::runtime_error(msg);}
+#define ASSERT_LOGIC(cond,msg) if(!(cond)){wxLogFatalError(_T("logic ")+ wxString(msg) );throw std::logic_error(msg);}
+#define ASSERT_RUNTIME(cond,msg) if(!(cond)){wxLogDebug(_T("runtime ")+ wxString(msg) );throw std::runtime_error(msg);}
 
 std::string i2s( int x );
 
 void debug_output( const std::string& prefix, const std::string& func, const std::string& params, const std::string& msg );
 
 
-#define debug_func( params ) debug_output( "**", __FUNCTION__, params, "" )
+#define debug_func( params ) wxLogTrace( __FUNCTION__ + _T(" ( ") + params + _T(" )"),,)
 
 
 #define boundry(var,min,max) var=(var<(min))?(min):(var>(max))?(max):var
