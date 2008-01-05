4ec9d5a0b63b01710e9159838531c4d985ec9ced
Andrea Piras <braindamage@springlobby.info>
2008-01-05 02:50:20 +0100

    now the app logs correctly the start position type in singleplayer scrip generation

--- src/spring.cpp
+++ src/spring.cpp
@@ -524,11 +524,10 @@ wxString Spring::GetSPScriptTxt( SinglePlayerBattle& battle )
 
   int NumAllys = 0;
   int PlayerTeam = -1;
-  int starttype;
 
   if ( usync()->VersionSupports( GF_XYStartPos ) && battle.GetStartType() == ST_Fixed ) battle.SetStartType( ST_Pick );
 
-  wxLogMessage( _T("StartPosType=") + WX_STRING( i2s( starttype ) ) );
+  wxLogMessage( _T("StartPosType=") + WX_STRING( i2s( battle.GetStartType() ) ) );
 
   for ( unsigned int i = 0; i < battle.GetNumBots(); i++ ) {
     BattleBot* bot;
