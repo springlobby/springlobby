764895c313868e38b36be94f274f8cd180b96427
Andrea Piras <braindamage@springlobby.info>
2008-01-11 16:18:23 +0100

    added some OS local defines for OSX

--- src/nonportable.h
+++ src/nonportable.h
@@ -1,7 +1,7 @@
 #ifndef SPRINGLOBBY_HEADERGUARD_NONPORTABLE_H
 #define SPRINGLOBBY_HEADERGUARD_NONPORTABLE_H
 
-#ifdef WIN32
+#ifdef WXMSW
   #define SPRING_VERSION_PARAM _T("/V")
   #define CHOOSE_EXE _("Executables (*.exe)|*.exe|Any File (*.*)|*.*")
   #define CHOOSE_DLL _("Library (*.dll)|*.dll|Any File (*.*)|*.*")
@@ -10,7 +10,7 @@
   #define SPRING_BIN _T("spring.exe")
   #define UNITSYNC_BIN _T("unitsync.dll")
   #define DOS_TXT true
-#else
+#elifdef WXGTK
   #define SPRING_VERSION_PARAM _T("-V")
   #define CHOOSE_EXE _("Any file (*)|*")
   #define CHOOSE_DLL _("Library (*.so)|*.so|Any File (*.*)|*.*")
@@ -18,6 +18,15 @@
   #define USYNC_CALL_CONV
   #define SPRING_BIN _T("spring")
   #define UNITSYNC_BIN _T("unitsync.so")
+  #define DOS_TXT false
+#elifdef WXMAC
+  #define SPRING_VERSION_PARAM _T("-V")
+  #define CHOOSE_EXE _("App Bundles (*.app)|*.app|Any File (*.*)|*.*")
+  #define CHOOSE_DLL _("Library (*.dylib)|*.dylib|Any File (*.*)|*.*")
+  #define DLL_EXTENSION _T(".dylib")
+  #define USYNC_CALL_CONV
+  #define SPRING_BIN _T("spring")
+  #define UNITSYNC_BIN _T("unitsync.dylib")
   #define DOS_TXT false
 #endif
 
