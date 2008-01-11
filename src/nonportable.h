59931d5f6cdf97c54866aafe912907365a332d86
Andrea Piras <braindamage@springlobby.info>
2008-01-11 18:06:40 +0100

    now AI detection is 100% cross platform

--- src/nonportable.h
+++ src/nonportable.h
@@ -5,7 +5,6 @@
   #define SPRING_VERSION_PARAM _T("/V")
   #define CHOOSE_EXE _("Executables (*.exe)|*.exe|Any File (*.*)|*.*")
   #define CHOOSE_DLL _("Library (*.dll)|*.dll|Any File (*.*)|*.*")
-  #define DLL_EXTENSION _T(".dll")
   #define USYNC_CALL_CONV __stdcall
   #define SPRING_BIN _T("spring.exe")
   #define UNITSYNC_BIN _T("unitsync.dll")
@@ -14,7 +13,6 @@
   #define SPRING_VERSION_PARAM _T("-V")
   #define CHOOSE_EXE _("Any file (*)|*")
   #define CHOOSE_DLL _("Library (*.so)|*.so|Any File (*.*)|*.*")
-  #define DLL_EXTENSION _T(".so")
   #define USYNC_CALL_CONV
   #define SPRING_BIN _T("spring")
   #define UNITSYNC_BIN _T("unitsync.so")
@@ -23,7 +21,6 @@
   #define SPRING_VERSION_PARAM _T("-V")
   #define CHOOSE_EXE _("App Bundles (*.app)|*.app|Any File (*.*)|*.*")
   #define CHOOSE_DLL _("Library (*.dylib)|*.dylib|Any File (*.*)|*.*")
-  #define DLL_EXTENSION _T(".dylib")
   #define USYNC_CALL_CONV
   #define SPRING_BIN _T("Spring.app")
   #define UNITSYNC_BIN _T("unitsync.dylib")
