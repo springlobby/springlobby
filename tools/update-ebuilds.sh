073bcfcdb63c7906b99890216d88facb6ef85d81
Leo Moisio <semi@springlobby.info>
2008-01-20 03:08:34 +0200

    script for updating ebuilds

---
+++ tools/update-ebuilds.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+cd $(dirname $0)/..
+
+if [ $# -ne 1 ] ; then
+	echo "Usage: update-ebuilds tarball-filename"
+	exit
+fi
+
+if [ x$(hostname) = xDelenn ] ; then
+	cd springlobby/gentoo/overlay/games-util/springlobby
+	git-pull
+	cp springlobby-template $1
+	ebuild $1 digest
+	git-add .
+	git-commit -m "automatic update of springlobby ebuild for automated tarball release"
+	git-push my-public
+else
+	ssh zi.fi ~/springlobby/tools/update-ebuilds.sh $*
+fi
+
