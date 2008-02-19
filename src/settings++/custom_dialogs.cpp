573024c29d2b80934b66f20d672c759b3c3f358e
koshi <koshi@springlobby.info>
2008-02-19 19:31:07 +0100

    icons not right yet

--- src/settings++/custom_dialogs.cpp
+++ src/settings++/custom_dialogs.cpp
@@ -9,6 +9,9 @@
 #include <wx/font.h>
 #include <wx/event.h>
 #include <wx/stattext.h>
+#include "utils.h"
+#include <wx/artprov.h>
+#include <wx/statbmp.h>
 
 #include "../images/springsettings.xpm"
 #include "../images/springlobby.xpm"
@@ -28,14 +31,88 @@ CustomMessageBox::CustomMessageBox(wxIcon* icon ,wxWindow *parent, const wxStrin
 			: wxDialog(parent,-1,caption,pos,wxDefaultSize,style|wxFRAME_FLOAT_ON_PARENT|wxDEFAULT_DIALOG_STYLE)
 {
 	SetIcon(*icon);
-	wxBoxSizer* box = new wxBoxSizer(wxVERTICAL);
+	/*wxBoxSizer* box = new wxBoxSizer(wxVERTICAL);
 	box->Add(0,15,1,wxEXPAND|wxALL);
 	box->Add(new wxStaticText(this, -1, message), 0, wxALL| wxEXPAND, 20);
 	box->Add(0,15,1,wxEXPAND|wxALL);
 	box->Add(CreateButtonSizer(style));
 
 	SetSizer(box);
-    box->SetSizeHints( this );
+    box->SetSizeHints( this );*/
+
+//******** copied from wxsource/generic/msgdlgg.cpp with small modifications***********************************************************
+//    SetMessageDialogStyle(style);
+
+        wxBoxSizer *topsizer = new wxBoxSizer( wxVERTICAL );
+
+    wxBoxSizer *icon_text = new wxBoxSizer( wxHORIZONTAL );
+
+
+    // 1) icon
+    if (style & wxICON_MASK)
+    {
+        wxBitmap bitmap;
+        switch ( style & wxICON_MASK )
+        {
+            default:
+                wxLogError(_T("wrong icon in msgbox specified"));
+                // fall through
+
+            case wxICON_ERROR:
+                bitmap = wxArtProvider::GetIcon(wxART_ERROR, wxART_MESSAGE_BOX);
+                break;
+
+            case wxICON_INFORMATION:
+                bitmap = wxArtProvider::GetIcon(wxART_INFORMATION, wxART_MESSAGE_BOX);
+                break;
+
+            case wxICON_WARNING:
+                bitmap = wxArtProvider::GetIcon(wxART_WARNING, wxART_MESSAGE_BOX);
+                break;
+
+            case wxICON_QUESTION:
+                bitmap = wxArtProvider::GetIcon(wxART_QUESTION, wxART_MESSAGE_BOX);
+                break;
+        }
+
+        wxStaticBitmap *icon = new wxStaticBitmap(this, wxID_ANY, bitmap);
+        icon_text->Add( icon, 0, wxCENTER );
+    }
+         wxBitmap bitmap = wxArtProvider::GetIcon(wxART_ERROR, wxART_MESSAGE_BOX);
+
+        wxStaticBitmap *icon = new wxStaticBitmap(this, wxID_ANY, bitmap);
+        icon_text->Add( icon, 0, wxCENTER );
+
+
+#if wxUSE_STATTEXT
+    // 2) text
+    icon_text->Add( CreateTextSizer( message ), 0, wxALIGN_CENTER | wxLEFT, 10 );
+
+    topsizer->Add( icon_text, 1, wxCENTER | wxLEFT|wxRIGHT|wxTOP, 10 );
+#endif // wxUSE_STATTEXT
+
+    // 3) buttons
+    int center_flag = wxEXPAND;
+    if (style & wxYES_NO)
+        center_flag = wxALIGN_CENTRE;
+    wxSizer *sizerBtn = CreateSeparatedButtonSizer(style & ButtonSizerFlags);
+    if ( sizerBtn )
+        topsizer->Add(sizerBtn, 0, center_flag | wxALL, 10 );
+
+    SetAutoLayout( true );
+    SetSizer( topsizer );
+
+    topsizer->SetSizeHints( this );
+    topsizer->Fit( this );
+    wxSize size( GetSize() );
+    if (size.x < size.y*3/2)
+    {
+        size.x = size.y*3/2;
+        SetSize( size );
+    }
+
+    Centre( wxBOTH | wxCENTER_FRAME);
+/***************************************************************************************************/
 }
 
 CustomMessageBox::~CustomMessageBox()
