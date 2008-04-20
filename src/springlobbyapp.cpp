622f6121108ad5d462b60160774be13db4b8e058
Andrea Piras <braindamage@springlobby.info>
2008-04-20 03:03:34 +0200

    fixed a string that should not be translated

--- src/springlobbyapp.cpp
+++ src/springlobbyapp.cpp
@@ -178,14 +178,14 @@ void SpringLobbyApp::SetupUserFolders()
        if ( result == 2 ) createdirs = false;
        else if ( result == 3 ) return;
 
-       if ( result == 0 ) dir = wxFileName::GetHomeDir() + _("/.spring");
+       if ( result == 0 ) dir = wxFileName::GetHomeDir() + _T("/.spring");
        else if ( result == 1 || result == 2 ) dir = wxDirSelector( _("Choose a folder"), wxFileName::GetHomeDir() + _T("/.spring") );
 
        if ( createdirs )
        {
          if ( dir.IsEmpty() || ( !wxFileName::Mkdir( dir ) || ( !wxFileName::Mkdir( dir + _T("/mods") ) || !wxFileName::Mkdir( dir + _T("/maps") ) || !wxFileName::Mkdir( dir + _T("/base") ) || !wxFileName::Mkdir( dir + _T("/demos") ) || !wxFileName::Mkdir( dir + _T("/screenshots")  ) ) ) )
          {
-           if ( dir.IsEmpty() ) dir = wxFileName::GetHomeDir() + _("/.spring");
+           if ( dir.IsEmpty() ) dir = wxFileName::GetHomeDir() + _T("/.spring");
           wxMessageBox( _("Something went wrong when creating the directories\nPlease create manually the following folders:") + wxString(_T("\n")) + dir +  _T("\n") + dir + _T("/mods\n") + dir + _T("/maps\n") + dir + _T("/base\n") );
          }
        }
