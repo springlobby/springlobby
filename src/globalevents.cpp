cc6624c857492e78a81c979829fde05e2fe372db
Rene Milk <koshi@springlobby.info>
2008-04-27 10:22:28 +0200

    same as last

---
+++ src/globalevents.cpp
@@ -0,0 +1,30 @@
+#include "globalevents.h"
+#include "settings++/custom_dialogs.h"
+
+DEFINE_EVENT_TYPE(httpDownloadEvtComplete)
+DEFINE_EVENT_TYPE(httpDownloadEvtFailed)
+
+BEGIN_EVENT_TABLE(SL_GlobalEvtHandler, wxEvtHandler)
+    EVT_COMMAND(wxID_ANY, httpDownloadEvtComplete,  SL_GlobalEvtHandler::OnHttpDownLoadComplete)
+    EVT_COMMAND(wxID_ANY, httpDownloadEvtFailed,    SL_GlobalEvtHandler::OnHttpDownLoadFailed)
+END_EVENT_TABLE()
+
+SL_GlobalEvtHandler& SL_GlobalEvtHandler::GetSL_GlobalEvtHandler()
+{
+    static SL_GlobalEvtHandler slGlobalEvtHandler;
+    return slGlobalEvtHandler;
+}
+
+SL_GlobalEvtHandler::SL_GlobalEvtHandler()
+{
+}
+
+void SL_GlobalEvtHandler::OnHttpDownLoadComplete(wxCommandEvent& event)
+{
+    customMessageBoxNoModal(SL_MAIN_ICON,event.GetString(),_("Download complete") );
+}
+
+void SL_GlobalEvtHandler::OnHttpDownLoadFailed(wxCommandEvent& event)
+{
+    customMessageBoxNoModal(SL_MAIN_ICON,event.GetString(),_("Download failed") );
+}
