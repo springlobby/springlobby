698477fcd4fd687806967c48f16e51890b9c685f
Andrea Piras <braindamage@springlobby.info>
2008-05-25 04:29:14 +0200

    fixed weird scrolling behaviour in win using normal textctrl

--- src/chatpanel.cpp
+++ src/chatpanel.cpp
@@ -494,6 +494,12 @@ void ChatPanel::OutputLine( const wxString& message, const wxColour& col, const 
 
 	#else
 	m_chatlog_text->AppendText( message + _T( "\n" ) );
+
+  if ( sett().GetSmartScrollEnabled() ) {
+    m_chatlog_text->ScrollLines( 10 ); /// to prevent for weird empty space appended
+		m_chatlog_text->ShowPosition( m_chatlog_text->GetLastPosition() );/// scroll to the bottom
+    m_chatlog_text->ScrollLines( 10 ); /// to prevent for weird empty space appended
+	}
 	#endif
 
 	CheckLength(); /// crop lines from history that exceeds limit
