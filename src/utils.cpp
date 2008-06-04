1516f39f634d146e041a58f30bd9adf1690c037d
Andrea Piras <braindamage@springlobby.info>
2008-06-04 21:51:09 +0200

    now console logging can be explictly activated an no log gets instantiated by default if no console logging is available

--- src/utils.cpp
+++ src/utils.cpp
@@ -60,13 +60,15 @@ void InitializeLoggingTargets()
     wxLogChain *logChain = new wxLogChain( loggerconsole );
     logChain->SetLogLevel( wxLOG_Trace );
     logChain->SetVerbose( true );
+  #elif defined ( USE_LOG_WINDOW )
+    ///gui window fallback logging if console/stream output not available
+    wxLog *loggerwin = new wxLogWindow(0, _T("SpringLobby error console")  );
+    wxLogChain *logChain = new wxLogChain( loggerwin );
+    logChain->SetLogLevel( wxLOG_Trace );
+    logChain->SetVerbose( true );
+    logChain->GetOldLog()->SetLogLevel( wxLOG_Warning );
   #else
-    /////gui window fallback logging if console/stream output not available
-    //wxLog *loggerwin = new wxLogWindow(0, _T("SpringLobby error console")  );
-    //wxLogChain *logChain = new wxLogChain( loggerwin );
-    //logChain->SetLogLevel( wxLOG_Trace );
-    //logChain->SetVerbose( true );
-    //logChain->GetOldLog()->SetLogLevel( wxLOG_Warning );
+    /// if all fails, no log is better than msg box spam :P
     new wxLogNull();
   #endif
 }
