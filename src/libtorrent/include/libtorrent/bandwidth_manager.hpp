f4ca23bd90538171aa5f875fcdae76a8e057c6f2
Tobi Vollebregt <tobivollebregt@gmail.com>
2008-06-12 14:51:56 +0200

    compiles in debug mode with boost 1.35 now

--- src/libtorrent/include/libtorrent/bandwidth_manager.hpp
+++ src/libtorrent/include/libtorrent/bandwidth_manager.hpp
@@ -132,7 +132,11 @@ struct bandwidth_manager
 
 	bool is_in_history(PeerConnection const* peer, boost::mutex::scoped_lock& l) const
 	{
+#if BOOST_VERSION >= 103500
+		TORRENT_ASSERT(l.owns_lock());
+#else
 		TORRENT_ASSERT(l.locked());
+#endif
 		for (typename history_t::const_iterator i
 			= m_history.begin(), end(m_history.end()); i != end; ++i)
 		{
@@ -265,7 +269,11 @@ private:
 
 	void hand_out_bandwidth(boost::mutex::scoped_lock& l)
 	{
+#if BOOST_VERSION >= 103500
+		TORRENT_ASSERT(l.owns_lock());
+#else
 		TORRENT_ASSERT(l.locked());
+#endif
 		// if we're already handing out bandwidth, just return back
 		// to the loop further down on the callstack
 		if (m_in_hand_out_bandwidth) return;
