aae09554b692f502f997764a501a1ca2808fa9f2
Rene Milk <koshi@springlobby.info>
2008-06-14 01:18:43 +0200

    fixes 519

--- src/chatpanel.cpp
+++ src/chatpanel.cpp
@@ -406,7 +406,7 @@ wxMenu* ChatPanel::CreateNickListMenu() {
 
 	m_user_menu->AppendSeparator();
 
-	if ( m_ui.GetServer().GetMe().GetStatus().moderator ) {
+	if ( ui().GetServer().GetMe().GetStatus().moderator ) {
 		wxMenuItem* modingameitem = new wxMenuItem( m_user_menu, CHAT_MENU_US_MODERATOR_INGAME, _( "Ingame time" ), wxEmptyString, wxITEM_NORMAL );
 		m_user_menu->Append( modingameitem );
 		wxMenuItem* modipitem = new wxMenuItem( m_user_menu, CHAT_MENU_US_MODERATOR_CURIP, _( "Retrieve IP and Smurfs" ), wxEmptyString, wxITEM_NORMAL );
