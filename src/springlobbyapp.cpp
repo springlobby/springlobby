195425962894b937329ce4532a575a0baa7d717a
Rene Milk <koshi@springlobby.info>
2008-08-05 19:09:38 +0200

    fixed doubled/wrong path seps in windows buld

--- src/springlobbyapp.cpp
+++ src/springlobbyapp.cpp
@@ -129,11 +129,11 @@ bool SpringLobbyApp::OnInit()
         SetupUserFolders();
 
         InitCacheDir();
-
+        wxString sep ( wxFileName::GetPathSeparator() );
         //! ask for downloading ota content if archive not found, start downloader in background
         wxString url= _T("ipxserver.dyndns.org/games/spring/mods/xta/base-ota-content.zip");
-        wxString destFilename = sett().GetSpringDir()+ wxFileName::GetPathSeparator()
-                +_T("base") + wxFileName::GetPathSeparator() + _T("base-ota-content.zip");
+        wxString destFilename = sett().GetSpringDir() + ( sett().GetSpringDir().EndsWith( sep ) ? _T("") : sep )
+                + _T("base") + sep + _T("base-ota-content.zip");
         bool contentExists = false;
         if ( usync()->IsLoaded() )
         {
