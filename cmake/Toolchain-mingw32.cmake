4b15e92ad31481844d2b459ba3f902020317b642
Rene Milk <koshi@springlobby.info>
2008-08-23 17:34:02 +0200

    win specifics back in toolcahin file

--- cmake/Toolchain-mingw32.cmake
+++ cmake/Toolchain-mingw32.cmake
@@ -15,8 +15,9 @@ SET(CMAKE_FIND_ROOT_PATH  /usr/bin/i586-mingw32msvc /var/lib/buildbot/lib/mingw 
 # SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 # SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 SET( wxWidgets_CONFIG_EXECUTABLE /var/lib/buildbot/lib/mingw/wx/2.8/bin/wx-config )
-SET( wxWidgets_INCLUDE_DIRS /var/lib/buildbot/lib/mingw/wx/2.8/include/wx-2.8 )
+SET( wxWidgets_INCLUDE_DIRS /var/lib/buildbot/lib/mingw/wx/2.8/include/wx-2.8 /var/lib/buildbot/lib/mingw/wx/2.8/lib/wx/include/i586-mingw32msvc-msw-unicode-release-2.8/)
 SET( wxWidgets_LIB_DIR /var/lib/buildbot/lib/mingw/wx/2.8/lib )
 SET( wxWidgets_CONFIGURATION mswu )
 SET( CMAKE_VERBOSE_MAKEFILE ON )
-# ADD_DEFINITIONS(-DWXUSINGDLL -D__WXMSW__)
+
+ADD_DEFINITIONS( -DWXUSINGDLL -D__WXMSW__)
