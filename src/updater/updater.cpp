22de42cb91f610c8740e245320ea3b73b85c9d18
Andrea Piras <braindamage@springlobby.info>
2008-08-26 22:03:47 +0200

    forgot to commit a couple of files >_>

---
+++ src/updater/updater.cpp
@@ -0,0 +1,81 @@
+#include "versionchecker.h"
+#include "../settings++/custom_dialogs.h"
+#include "../utils.h"
+#include "exedownloader.h"
+#include "updater.h"
+
+#include <wx/stdpaths.h>
+#include <wx/filefn.h>
+#include <wx/filename.h>
+
+UpdaterClass& Updater()
+{
+    static UpdaterClass m_upd;
+    return m_upd;
+}
+
+
+UpdaterClass::UpdaterClass()
+{
+}
+
+
+UpdaterClass::~UpdaterClass()
+{
+  delete m_exedownloader;
+}
+
+void UpdaterClass::CheckForUpdates()
+{
+  wxString latestVersion = GetLatestVersion();
+  // Need to replace crap chars or versions will always be inequal
+  latestVersion.Replace(_T(" "), _T(""), true);
+  latestVersion.Replace(_T("\n"), _T(""), true);
+  latestVersion.Replace(_T("\t"), _T(""), true);
+  if (latestVersion == _T("-1"))
+  {
+    customMessageBoxNoModal(SL_MAIN_ICON, _("There was an error checking for the latest version.\nPlease try again later.\nIf the problem persists, please use Help->Report Bug to report this bug."), _("Error"));
+    return;
+  }
+  wxString myVersion = GetSpringLobbyVersion();
+
+  wxString msg = _("Your Version: ") + myVersion + _T("\n") + _("Latest Version: ") + latestVersion;
+
+  if ( !latestVersion.IsSameAs(myVersion, false) )
+  {
+    int answer = customMessageBox(SL_MAIN_ICON, _("Your SpringLobby version is not up to date.\n\n") + msg + _("\n\nWould you like for me to autodownload the new version? It will be automatically used next time you launch the lobby again."), _("Not up to Date"), wxYES_NO);
+    if (answer == wxYES)
+    {
+      wxChar sep = wxFileName::GetPathSeparator();
+      wxString currentexe = wxStandardPaths::Get().GetExecutablePath();
+      m_newexe = currentexe.AfterLast( sep ) + _T("SpringLobbyUpdate") + sep;
+      m_exedownloader = new ExeDownloader( _T("foo"), m_newexe );
+    }
+  }
+}
+
+void UpdaterClass::OnDownloadEvent( int code )
+{
+  if ( code != 0) customMessageBoxNoModal(SL_MAIN_ICON, _("There was an error downloading for the latest version.\nPlease try again later.\nIf the problem persists, please use Help->Report Bug to report this bug."), _("Error"));
+  else
+  {
+    if ( UpdateExe( m_newexe + _T("SpringLobby.exe"), false ) )  customMessageBoxNoModal(SL_MAIN_ICON, _("There was an error while trying to replace the current executable version\n manual copy is necessary from\n") + m_newexe + _("to\n") + wxStandardPaths::Get().GetExecutablePath() +  _("\nPlease use Help->Report Bug to report this bug."), _("Error"));
+  }
+}
+
+
+bool UpdaterClass::UpdateExe( const wxString& newexe, bool WaitForReboot )
+{
+  if ( !wxFileName::IsFileExecutable( newexe ) ) return false;
+  wxChar sep = wxFileName::GetPathSeparator();
+  wxString currentexe = wxStandardPaths::Get().GetExecutablePath();
+  wxString backupfile =  currentexe.BeforeLast( sep ) + _T("oldlobby.bak");
+  if ( !wxRenameFile( currentexe, backupfile ) ) return false;
+  if ( !wxRenameFile( newexe, currentexe ) )
+  {
+    wxRenameFile( backupfile, currentexe );
+    return false;
+  }
+  wxRemoveFile( backupfile );
+  return true;
+}
