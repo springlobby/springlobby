810562d3dfc57c7da3d168de785133418bfb24de
Andrea Piras <braindamage@springlobby.info>
2008-09-07 22:30:42 +0200

    fixed buildbot script

--- tools/buildbot-create-build-dirs.sh
+++ tools/buildbot-create-build-dirs.sh
@@ -17,11 +17,18 @@ create-build-dir ()
 	echo -n configuring $1 with $2 ...
 	(
 	cd $1
-	cmake $2 ..
-        )
+	if [ ! -f Makefile ] ; then
+		../configure --config-cache $2
+		echo done
+	else
+		# regenerate Makefile with current config options
+		./config.status
+		echo regen done
+	fi
+	)
 }
 
-#create-build-dir build
-create-build-dir build-linux-wx28 
-create-build-dir build-mingw-wx28 "-DCMAKE_TOOLCHAIN_FILE=../cmake/Toolchain-mingw32.cmake"
-    
+create-build-dir build
+create-build-dir build-linux-wx28 "--with-wx-config=/var/lib/buildbot/lib/linux/wx/2.8/bin/wx-config "
+create-build-dir build-mingw-wx28 "--build=i686-pc-linux-gnu --host=i586-mingw32msvc --with-wx-config=/var/lib/buildbot/lib/mingw/wx/2.8/bin/wx-config  --with-boost-prefix=/var/lib/buildbot/lib/mingw/include/  --with-sdl-config=/var/lib/buildbot/lib/mingw/bin/sdl-config "
+
