d7fa4e7894ef74ea02c0cfc8ae8fcda56dedc120
Andrea Piras <braindamage@springlobby.info>
2008-10-20 21:10:42 +0200

    re-added old stacktrace code

---
+++ src/stacktrace.cpp
@@ -0,0 +1,58 @@
+/* Copyright (C) 2007 The SpringLobby Team. All rights reserved. */
+//
+// Class: StackTrace
+//
+
+
+#include "stacktrace.h"
+#include "utils.h"
+
+
+#if wxUSE_STACKWALKER
+
+
+StackTrace& stacktrace() { static StackTrace trace; return trace; };
+
+
+void StackTrace::OnStackFrame ( const wxStackFrame& frame )
+{
+  StackTraceString += wxString::Format( _T("(%d) "), frame.GetLevel() ); // (frame_level_number)
+  PartToHash += wxString::Format( _T("(%d) "), frame.GetLevel() );
+  StackTraceString += wxString::Format( _T("[%p] "), frame.GetAddress() ); // [calling_address]
+
+  if ( frame.HasSourceLocation() )
+  {
+    StackTraceString += frame.GetName();  // function_name
+    PartToHash += frame.GetName() + _T("\n");
+
+    int paramcount = frame.GetParamCount();
+    if ( paramcount == 0 ){
+      StackTraceString += _T("\n");
+    } else {
+      StackTraceString += _T("(");
+      while ( paramcount > 0 )
+      {
+        wxString type;
+        wxString name;
+        wxString value;
+
+        if ( frame.GetParam( paramcount, &type, &name, &value) ) // data_type var_name = value,
+        {
+          StackTraceString += _T(" ") + type;
+          StackTraceString += _T(" ") + name;
+          StackTraceString += _T(" = ") + value;
+        }
+
+        paramcount--;
+        if ( paramcount > 0 ) StackTraceString += _T(",");
+      }
+    }
+    StackTraceString += _T("->") + frame.GetFileName() + wxString::Format( _T(":%d"), frame.GetLine() ); // File: file_name : line_number
+
+  }
+  StackTraceString += _T("\n");
+
+}
+
+
+#endif
---
+++ src/stacktrace.h
@@ -0,0 +1,30 @@
+#ifndef STACKTRACE_H_INCLUDED
+#define STACKTRACE_H_INCLUDED
+
+#include <wx/string.h>
+#include <wx/stackwalk.h>
+
+#if wxUSE_STACKWALKER
+
+class StackTrace : public wxStackWalker
+{
+  public:
+    StackTrace() {}
+    ~StackTrace() {}
+
+    wxString GetStackTrace() { return StackTraceString; }
+
+  private:
+
+    void OnStackFrame( const wxStackFrame& frame );
+
+    wxString StackTraceString;
+    wxString PartToHash;
+
+};
+
+StackTrace& stacktrace();
+
+#endif
+
+#endif // SPRINGLOBBY_HEADERGUARD_STACKTRACE_H_INCLUDED
