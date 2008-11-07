1c22af42c26c6e3cf15f5165e69557681f87dbbe
Rene Milk <koshi@springlobby.info>
2008-11-07 21:27:31 +0100

    first draft of update script for translations

---
+++ tools/update-translations-buildbot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+cd $(dirname $0)/..
+
+#git remote add translations root@springlobby.info:/home/pootle/translations/
+git fetch translations 
+git merge translations/master
+make -C po update-po && make -C po update-pot && git commit -a -m "auto-updated translations"
