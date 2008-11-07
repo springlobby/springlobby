13a1073c939697aefbfa8ebb7a651ae4c6a30003
Rene Milk <koshi@springlobby.info>
2008-11-07 21:50:14 +0100

    added exit 0 to  update script for translations

--- tools/update-translations-buildbot.sh
+++ tools/update-translations-buildbot.sh
@@ -7,3 +7,6 @@ cd $(dirname $0)/..
 git fetch translations 
 git merge translations/master
 make -C po update-po && make -C po update-pot && git commit -a -m "auto-updated translations"
+
+#for good measure
+exit 0
