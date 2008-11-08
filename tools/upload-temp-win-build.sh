1b0e911d109ee330be9721094f801f71605fcadd
Rene Milk <koshi@springlobby.info>
2008-11-08 02:01:47 +0100

    get branch name from buildbot

--- tools/upload-temp-win-build.sh
+++ tools/upload-temp-win-build.sh
@@ -5,8 +5,8 @@ cd $(dirname $0)/../$1
 pwd
 
 /usr/i586-mingw32msvc/bin/strip springlobby.exe
-branch=$((git branch | grep "*") | sed -e "s;*\ ;;g" -e "s;\n;;g")
-filename=sl_${branch}.exe
+#branch=$((git branch | grep "*") | sed -e "s;*\ ;;g" -e "s;\n;;g")
+filename=sl_${2}.exe
 scp springlobby.exe graasmilk.net:springlobby.info/temp/builds/${filename}
 ssh rg chown www-data:wwwdata /usr/local/www/springlobby.info/temp/builds/${filename}
 springlobby.info/temp/builds/${filename}
