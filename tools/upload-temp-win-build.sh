062e8f3125029bb6cff2406aa20929de3a537f2f
Rene Milk <koshi@springlobby.info>
2008-11-08 01:06:25 +0100

    added branchname to exe for temp bulds

--- tools/upload-temp-win-build.sh
+++ tools/upload-temp-win-build.sh
@@ -4,5 +4,7 @@ set -e
 cd $(dirname $0)/../$1
 
 /usr/i586-mingw32msvc/bin/strip *.exe
-scp springlobby.exe graasmilk.net:springlobby.info/temp/builds/sl.exe
-echo "http://springlobby.info/temp/builds/$1.exe"
+branch=(git branch | grep "*") | sed -e "s;*\ ;;g" -e "s;\n;;g"
+filename=sl_${branch}.exe
+scp springlobby.exe graasmilk.net:springlobby.info/temp/builds/${filename}
+echo "http://springlobby.info/temp/builds/${filename}"
