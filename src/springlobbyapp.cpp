1cdb4a30f9d0cd3d2b8521f83882948de13131c4
Andrea Piras <braindamage@springlobby.info>
2008-12-14 22:36:16 +0100

    typo fix

--- src/springlobbyapp.cpp
+++ src/springlobbyapp.cpp
@@ -165,7 +165,7 @@ bool SpringLobbyApp::OnInit()
             m_otadownloader = new HttpDownloader( url, destFilename );
         }
 
-        customMessageBoxNoModal(SL_MAIN_ICON, _("By default SpringLobby reports some statistics.?\n"
+        customMessageBoxNoModal(SL_MAIN_ICON, _("By default SpringLobby reports some statistics.\n"
                                                  "You can disable that on options tab --> General."),_("Notice"),wxOK );
         ui().mw().ShowConfigure();
     }
