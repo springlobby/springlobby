f45b54648eaf0efb037941e74a16a99d91c28686
Tobi Vollebregt <tobivollebregt@gmail.com>
2008-12-21 12:45:24 +0100

    Working prototype map grid.

--- src/maplistctrl.cpp
+++ src/maplistctrl.cpp
@@ -1,2 +1,120 @@
 /* Copyright (C) 2007 The SpringLobby Team. All rights reserved. */
 
+#include "maplistctrl.h"
+
+#include "utils.h"
+#include <wx/dcclient.h>
+#include <wx/image.h>
+
+
+/// Size of the map previews.  This should be same as size of map previews in
+/// battle list and as prefetch size in SpringUnitSync for performance reasons.
+#define MINIMAP_SIZE 98
+
+
+BEGIN_EVENT_TABLE( MapListCtrl, wxPanel )
+  EVT_PAINT( MapListCtrl::OnPaint )
+  EVT_SIZE( MapListCtrl::OnResize )
+  EVT_COMMAND( wxID_ANY, UnitSyncGetMapImageAsyncCompletedEvt, MapListCtrl::OnGetMapImageAsyncCompleted )
+END_EVENT_TABLE()
+
+
+MapListCtrl::MapListCtrl( wxWindow* parent, int size, IBattle* battle, Ui& ui )
+	: wxPanel( parent, -1, wxDefaultPosition, wxSize(size, size), wxSIMPLE_BORDER|wxFULL_REPAINT_ON_RESIZE )
+	, m_ui( ui )
+	, m_battle( battle )
+{
+}
+
+
+MapListCtrl::~MapListCtrl()
+{
+
+}
+
+
+void MapListCtrl::LoadMaps()
+{
+	wxArrayString maps = usync().GetMapList();
+	int count = maps.size();
+
+	wxLogDebugFunc( TowxString( maps.size() ) );
+
+	m_maps.clear();
+
+	for (int i = 0; i < count; ++i ) {
+		try {
+			m_maps[maps[i]] = usync().GetMapEx( maps[i] );
+		}
+		catch (...) {}
+	}
+}
+
+
+void MapListCtrl::SetBattle( IBattle* battle )
+{
+	m_battle = battle;
+}
+
+
+void MapListCtrl::DrawMap( wxDC& dc, MapData& map, int x, int y )
+{
+	switch ( map.state ) {
+		case MapState_NoMinimap:
+			usync().GetMinimapAsync( map.name, MINIMAP_SIZE, MINIMAP_SIZE, this );
+			map.state = MapState_GetMinimap;
+			break;
+		case MapState_GetMinimap:
+			// do nothing, waiting for async fetch of minimap
+			break;
+		case MapState_GotMinimap:
+			x += (MINIMAP_SIZE - map.minimap.GetWidth()) / 2;
+			y += (MINIMAP_SIZE - map.minimap.GetHeight()) / 2;
+			dc.DrawBitmap( map.minimap, x, y, false );
+			break;
+	}
+}
+
+
+void MapListCtrl::OnPaint( wxPaintEvent& event )
+{
+	wxLogDebugFunc( _T("") );
+
+	if ( m_maps.size() == 0 ) LoadMaps();
+
+	wxPaintDC dc( this );
+
+	int width, height;
+	GetClientSize( &width, &height );
+
+	std::map<wxString, MapData>::iterator it = m_maps.begin();
+	if (it == m_maps.end()) return;
+
+	const int size = MINIMAP_SIZE + 1;
+
+	for (int y = (height % size) / 2; y <= height - size; y += size) {
+		for (int x = (width % size) / 2; x <= width - size; x += size) {
+			DrawMap( dc, it->second, x, y );
+			++it;
+			if (it == m_maps.end()) return;
+		}
+	}
+}
+
+
+void MapListCtrl::OnResize( wxSizeEvent& event )
+{
+}
+
+
+void MapListCtrl::OnGetMapImageAsyncCompleted( wxCommandEvent& event )
+{
+	wxString mapname = event.GetString();
+
+	wxLogDebugFunc( mapname );
+
+	m_maps[mapname].minimap = usync().GetMinimap( mapname, MINIMAP_SIZE, MINIMAP_SIZE );
+	m_maps[mapname].state = MapState_GotMinimap;
+
+	Refresh(); // TODO: use RefreshRect ?
+}
--- src/maplistctrl.h
+++ src/maplistctrl.h
@@ -1,4 +1,55 @@
 #ifndef SPRINGLOBBY_HEADERGUARD_MAPLISTCTRL_H
 #define SPRINGLOBBY_HEADERGUARD_MAPLISTCTRL_H
 
+#include "iunitsync.h"
+#include <wx/bitmap.h>
+#include <wx/panel.h>
+
+class IBattle;
+class Ui;
+
+
+class MapListCtrl : public wxPanel
+{
+	public:
+		MapListCtrl( wxWindow* parent, int size, IBattle* battle, Ui& ui );
+		~MapListCtrl();
+
+		void SetBattle( IBattle* battle );
+
+		void OnPaint( wxPaintEvent& event );
+		void OnResize( wxSizeEvent& event );
+
+		void OnGetMapImageAsyncCompleted( wxCommandEvent& event );
+
+	protected:
+
+		enum MapState
+		{
+			MapState_NoMinimap,
+			MapState_GetMinimap,
+			MapState_GotMinimap
+		};
+
+		struct MapData : UnitSyncMap
+		{
+			MapData() : state( MapState_NoMinimap ) {}
+			void operator=( const UnitSyncMap& other ) { UnitSyncMap::operator=( other ); }
+
+			wxBitmap minimap;
+			MapState state;
+		};
+
+		void LoadMaps();
+		void DrawMap( wxDC& dc, MapData& map, int x, int y );
+
+		Ui& m_ui;
+		IBattle* m_battle;
+
+		std::map< wxString, MapData > m_maps;
+		std::vector< std::vector<MapData*> > m_bins;
+
+		DECLARE_EVENT_TABLE();
+};
+
 #endif // SPRINGLOBBY_HEADERGUARD_MAPLISTCTRL_H
