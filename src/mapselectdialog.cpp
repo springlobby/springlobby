a540cc9ec929277d979c07ec65e193f35d10f6ce
Tobi Vollebregt <tobivollebregt@gmail.com>
2008-12-26 16:25:58 +0100

    Minor bit of code for MapSelectDialog.

--- src/mapselectdialog.cpp
+++ src/mapselectdialog.cpp
@@ -1,2 +1,20 @@
 /* Copyright (C) 2007 The SpringLobby Team. All rights reserved. */
+/* Author: Tobi Vollebregt */
 
+#include "mapselectdialog.h"
+
+
+BEGIN_EVENT_TABLE( MapSelectDialog, wxDialog )
+END_EVENT_TABLE()
+
+
+MapSelectDialog::MapSelectDialog( wxWindow* parent, Ui& ui )
+	: wxDialog( parent, -1, _("Select map"), wxDefaultPosition, wxSize(300, 300), wxDEFAULT_DIALOG_STYLE | wxCLIP_CHILDREN )
+	, m_ui(ui)
+{
+}
+
+
+MapSelectDialog::~MapSelectDialog()
+{
+}
--- src/mapselectdialog.h
+++ src/mapselectdialog.h
@@ -1,4 +1,25 @@
+/* Author: Tobi Vollebregt */
+
 #ifndef SPRINGLOBBY_HEADERGUARD_MAPSELECTDIALOG_H
 #define SPRINGLOBBY_HEADERGUARD_MAPSELECTDIALOG_H
+
+#include <wx/dialog.h>
+
+class Ui;
+
+
+class MapSelectDialog : public wxDialog
+{
+	public:
+
+		MapSelectDialog( wxWindow* parent, Ui& ui );
+		~MapSelectDialog();
+
+	protected:
+
+		Ui& m_ui;
+
+		DECLARE_EVENT_TABLE();
+};
 
 #endif // SPRINGLOBBY_HEADERGUARD_MAPSELECTDIALOG_H
