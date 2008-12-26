84a5e9ba00c93b6afda24e890c165e9ecbe1df49
Tobi Vollebregt <tobivollebregt@gmail.com>
2008-12-26 22:02:53 +0100

    Initial attempt at MapSelectDialog in wxSmith.  Very WIP!

--- src/mapselectdialog.cpp
+++ src/mapselectdialog.cpp
@@ -1,21 +1,60 @@
-/* Copyright (C) 2007 The SpringLobby Team. All rights reserved. */
-/* Author: Tobi Vollebregt */
-
 #include "mapselectdialog.h"
 
+//(*InternalHeaders(MapSelectDialog)
+#include <wx/sizer.h>
+#include <wx/stattext.h>
+#include <wx/choice.h>
+#include <wx/intl.h>
+#include <wx/button.h>
+#include <wx/string.h>
+#include <mapgridctrl.h>
+//*)
 
-BEGIN_EVENT_TABLE( MapSelectDialog, wxDialog )
+//(*IdInit(MapSelectDialog)
+const long MapSelectDialog::ID_STATICTEXT1 = wxNewId();
+const long MapSelectDialog::ID_CHOICE1 = wxNewId();
+const long MapSelectDialog::ID_MAPGRID = wxNewId();
+//*)
+
+BEGIN_EVENT_TABLE(MapSelectDialog,wxDialog)
+	//(*EventTable(MapSelectDialog)
+	//*)
 END_EVENT_TABLE()
 
-
-MapSelectDialog::MapSelectDialog( wxWindow* parent, Ui& ui )
-	: wxDialog( parent, -1, _("Select map"), wxDefaultPosition, wxSize(640, 480),
-			wxDEFAULT_DIALOG_STYLE | wxCLIP_CHILDREN | wxRESIZE_BORDER | wxMAXIMIZE_BOX)
-	, m_ui(ui)
+MapSelectDialog::MapSelectDialog(wxWindow* parent,Ui& ui)
+	: m_ui(ui)
 {
-}
+	//(*Initialize(MapSelectDialog)
+	wxBoxSizer* BoxSizer2;
+	wxBoxSizer* BoxSizer1;
+	wxStdDialogButtonSizer* StdDialogButtonSizer1;
 
+	Create(parent, wxID_ANY, _("Select map"), wxDefaultPosition, wxDefaultSize, wxDEFAULT_DIALOG_STYLE|wxRESIZE_BORDER, _T("wxID_ANY"));
+	SetClientSize(wxSize(800,600));
+	BoxSizer1 = new wxBoxSizer(wxHORIZONTAL);
+	BoxSizer2 = new wxBoxSizer(wxVERTICAL);
+	StaticText1 = new wxStaticText(this, ID_STATICTEXT1, _("Label"), wxDefaultPosition, wxDefaultSize, 0, _T("ID_STATICTEXT1"));
+	BoxSizer2->Add(StaticText1, 0, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+	Choice1 = new wxChoice(this, ID_CHOICE1, wxDefaultPosition, wxDefaultSize, 0, 0, 0, wxDefaultValidator, _T("ID_CHOICE1"));
+	BoxSizer2->Add(Choice1, 0, wxALL|wxEXPAND|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+	BoxSizer2->Add(0,0,1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+	StdDialogButtonSizer1 = new wxStdDialogButtonSizer();
+	StdDialogButtonSizer1->AddButton(new wxButton(this, wxID_CANCEL, wxEmptyString));
+	StdDialogButtonSizer1->AddButton(new wxButton(this, wxID_APPLY, wxEmptyString));
+	StdDialogButtonSizer1->Realize();
+	BoxSizer2->Add(StdDialogButtonSizer1, 0, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+	BoxSizer1->Add(BoxSizer2, 0, wxALL|wxEXPAND|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+	m_mapgrid = new MapGridCtrl(this,m_ui);
+	BoxSizer1->Add(m_mapgrid, 1, wxALL|wxEXPAND|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+	SetSizer(BoxSizer1);
+	BoxSizer1->SetSizeHints(this);
+	Center();
+	//*)
+}
 
 MapSelectDialog::~MapSelectDialog()
 {
+	//(*Destroy(MapSelectDialog)
+	//*)
 }
+
--- src/mapselectdialog.h
+++ src/mapselectdialog.h
@@ -1,25 +1,44 @@
-/* Author: Tobi Vollebregt */
+#ifndef MAPSELECTDIALOG_H
+#define MAPSELECTDIALOG_H
 
-#ifndef SPRINGLOBBY_HEADERGUARD_MAPSELECTDIALOG_H
-#define SPRINGLOBBY_HEADERGUARD_MAPSELECTDIALOG_H
-
+//(*Headers(MapSelectDialog)
 #include <wx/dialog.h>
+class wxStdDialogButtonSizer;
+class MapGridCtrl;
+class wxStaticText;
+class wxBoxSizer;
+class wxChoice;
+//*)
 
 class Ui;
 
-
-class MapSelectDialog : public wxDialog
+class MapSelectDialog: public wxDialog
 {
 	public:
 
-		MapSelectDialog( wxWindow* parent, Ui& ui );
-		~MapSelectDialog();
+		MapSelectDialog(wxWindow* parent,Ui& ui);
+		virtual ~MapSelectDialog();
 
 	protected:
 
+		//(*Declarations(MapSelectDialog)
+		MapGridCtrl* m_mapgrid;
+		wxStaticText* StaticText1;
+		wxChoice* Choice1;
+		//*)
+
+		//(*Identifiers(MapSelectDialog)
+		static const long ID_STATICTEXT1;
+		static const long ID_CHOICE1;
+		static const long ID_MAPGRID;
+		//*)
+
+		//(*Handlers(MapSelectDialog)
+		//*)
+
 		Ui& m_ui;
 
-		DECLARE_EVENT_TABLE();
+		DECLARE_EVENT_TABLE()
 };
 
-#endif // SPRINGLOBBY_HEADERGUARD_MAPSELECTDIALOG_H
+#endif
