6cd5c5e25a8031a9389cc71fe2ca2979d3cc8f69
Rene Milk <koshi@springlobby.info>
2008-12-30 18:07:10 +0100

    speedup buildslave compile by 40%

--- tools/buildbot-create-build-dirs.sh
+++ tools/buildbot-create-build-dirs.sh
@@ -46,7 +46,7 @@ create-build-dir ()
 	)
 }
 
-create-build-dir build
-create-build-dir build-linux-wx28 "--with-wx-config=/var/lib/buildbot/lib/linux/wx/2.8/bin/wx-config "
-create-build-dir build-mingw-wx28 "--build=i686-pc-linux-gnu --host=i586-mingw32msvc --with-wx-config=/var/lib/buildbot/lib/mingw/wx/2.8/bin/wx-config  --with-boost-prefix=/var/lib/buildbot/lib/mingw/include/  --with-sdl-config=/var/lib/buildbot/lib/mingw/bin/sdl-config "
+create-build-dir build "CXXFLAGS=\"-O0\""
+create-build-dir build-linux-wx28 "--with-wx-config=/var/lib/buildbot/lib/linux/wx/2.8/bin/wx-config CXXFLAGS=\"-O0\""
+create-build-dir build-mingw-wx28 "--build=i686-pc-linux-gnu --host=i586-mingw32msvc --with-wx-config=/var/lib/buildbot/lib/mingw/wx/2.8/bin/wx-config  --with-boost-prefix=/var/lib/buildbot/lib/mingw/include/  --with-sdl-config=/var/lib/buildbot/lib/mingw/bin/sdl-config CXXFLAGS=\"-O0\""
 create-build-dir-cmake build-cmake
