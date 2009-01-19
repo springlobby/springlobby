c7dee8006a1dc7615c1f52de87dab6ad218402b1
Andrea Piras <braindamage@springlobby.info>
2009-01-19 03:57:16 +0100

    don't spam console with mod not found message

--- src/replay/replaylist.cpp
+++ src/replay/replaylist.cpp
@@ -227,8 +227,11 @@ void ReplayList::GetBattleFromScript( const wxString& script_, OfflineBattle& ba
             if ( player.ok() )
             {
                 User user ( player->GetString( _T("name") ), (player->GetString( _T("countryCode")).Upper() ), 0);
-								wxArrayString sides = usync().GetSides( battle.LoadMod().name );
-                user.BattleStatus().side = sides.Index( player->GetString( _T("side") ) );
+                if ( battle.ModExists() )
+                {
+									wxArrayString sides = usync().GetSides( battle.LoadMod().name );
+									user.BattleStatus().side = sides.Index( player->GetString( _T("side") ) );
+                }
                 user.BattleStatus().spectator = player->GetInt( _T("Spectator"), 0 );
                 opts.spectators += user.BattleStatus().spectator;
                 user.BattleStatus().team = player->GetInt( _T("team") );
