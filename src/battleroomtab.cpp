a24fddd88dbf1e64091fc268379379b03657a5e0
Andrea Piras <braindamage@springlobby.info>
2009-02-16 14:25:05 +0100

    don't allow the user change map size :P

--- src/battleroomtab.cpp
+++ src/battleroomtab.cpp
@@ -843,6 +843,7 @@ void BattleRoomTab::OnOptionActivate( wxListEvent& event )
 {
 	if ( !m_battle.IsFounderMe() ) return;
 	long index = event.GetIndex();
+	if ( index == 0 ) return;
 	wxString tag;
 	for ( OptionListMap::iterator itor = m_opt_list_map.begin(); itor != m_opt_list_map.end(); itor++ )
 	{
