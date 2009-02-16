96d92dd8bcaf155aca887757ce47208c050d7122
Andrea Piras <braindamage@springlobby.info>
2009-02-16 23:49:49 +0100

    removed #springlobby autojoin removal

--- src/springlobbyapp.cpp
+++ src/springlobbyapp.cpp
@@ -197,14 +197,6 @@ bool SpringLobbyApp::OnInit()
 				 sett().SetServer( _T("Backup server 2"), _T("springbackup2.servegame.org"), 8200 );
 				 sett().SetServer( _T("Test server"), _T("taspringmaster.servegame.com"), 8300 );
 			}
-			if ( sett().GetSettingsVersion() < 9 )
-			{
-				if ( sett().GetChannelJoinIndex( _T("springlobby" ) ) != -1 )
-				{
-					sett().RemoveChannelJoin( _T("springlobby") );
-					sett().AddChannelJoin( _T("main"), _T("") );
-				}
-			}
     }
 
     ui().ReloadUnitSync(); // first time load of unitsync
