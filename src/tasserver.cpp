da1611893c91f118feb3184535c2650ecae2f935
Andrea Piras <braindamage@springlobby.info>
2009-03-12 18:01:36 +0100

    make register answer check be \r\n tolerant too

--- src/tasserver.cpp
+++ src/tasserver.cpp
@@ -358,12 +358,12 @@ bool TASServer::Register( const wxString& addr, const int port, const wxString& 
     m_sock->Connect( addr, port );
     if ( !IsConnected() ) return false;
 
-    wxString data = m_sock->Receive().BeforeLast(_T('\n'));
+    wxString data = m_sock->Receive().BeforeLast(_T('\n')).BeforeLast(_T('\r'));
     if ( GetWordParam( data ) != _T("TASServer") ) return false;
 
     SendCmd( _T("REGISTER"), nick + _T(" ") + GetPasswordHash( password ) );
 
-    data = m_sock->Receive().BeforeLast(_T('\n'));
+    data = m_sock->Receive().BeforeLast(_T('\n')).BeforeLast(_T('\r'));
     if ( data.IsEmpty() )
     {
         reason = _("Connection timed out");
