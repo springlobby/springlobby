a24a6720c033bb3a6f42f5c4e5a0fa0dcefdf015
Andrea Piras <braindamage@springlobby.info>
2009-04-25 19:11:17 +0200

    add separators in right click popup menu in the battleroom

--- src/battleroomlistctrl.cpp
+++ src/battleroomlistctrl.cpp
@@ -102,6 +102,7 @@ BattleroomListCtrl::BattleroomListCtrl( wxWindow* parent, IBattle* battle, Ui& u
 	{
 		m_popup = new UserMenu(this);
 
+		m_popup->AppendSeparator();
 		wxMenu* m_rank = new wxMenu();
 
 		for ( unsigned int i = (unsigned int)UserStatus::USER_RANK_UNKNOWN; i < (unsigned int)UserStatus::USER_RANK_10; i++ )
@@ -122,6 +123,7 @@ BattleroomListCtrl::BattleroomListCtrl( wxWindow* parent, IBattle* battle, Ui& u
 		}
 		m_popup->Append( -1, _("Set Trust"), m_trust );
 
+		m_popup->AppendSeparator();
 		wxMenu* m_teams;
 		m_teams = new wxMenu();
 
