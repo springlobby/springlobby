399f85d9d9076785972d7825575e4a0676ae76d6
Rene Milk <koshi@springlobby.info>
2009-05-19 21:56:28 +0200

    Revert "next try"
    This reverts commit 82e06efac4202e749c9bdb1feecc951c49c4181d.
    
    Conflicts:
    
    	tools/buildbot-create-build-dirs.sh

--- tools/buildbot-create-build-dirs.sh
+++ tools/buildbot-create-build-dirs.sh
@@ -47,6 +47,5 @@ create-build-dir ()
 	)
 }
 
-DIR=$(pwd)
 create-build-dir-cmake build "-DOPTION_OPT_LEVEL=0 -DwxWidgets_CONFIG_EXECUTABLE=/var/lib/buildbot/lib/linux/wx/2.8/bin/wx-config"
-create-build-dir-cmake build-mingw-wx28 "-DCMAKE_TOOLCHAIN_FILE=${DIR}/cmake/Toolchain-mingw32.cmake"
+create-build-dir-cmake build-mingw-wx28 "-DCMAKE_TOOLCHAIN_FILE=${PWD}/cmake/Toolchain-mingw32.cmake"
