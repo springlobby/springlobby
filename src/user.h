89095762b486f7dc61e996afc2ba19d5ac285be5
Andrea Piras <braindamage@springlobby.info>
2009-05-28 16:57:39 +0200

    fixed initialization order ( removes warnings)

--- src/user.h
+++ src/user.h
@@ -64,7 +64,7 @@ struct UserBattleStatus
   wxString ip;
   unsigned int udpport;
   bool IsBot() const { return !aishortname.IsEmpty(); }
-  UserBattleStatus(): team(0),ally(0),colour(wxColour(0,0,0)),color_index(-1),handicap(0),side(0),sync(SYNC_UNKNOWN),spectator(false),ready(false), isfromdemo(false), udpport(0),aitype(-1) {}
+  UserBattleStatus(): team(0),ally(0),colour(wxColour(0,0,0)),color_index(-1),handicap(0),side(0),sync(SYNC_UNKNOWN),spectator(false),ready(false), isfromdemo(false), aitype(-1), udpport(0) {}
   bool operator == ( const UserBattleStatus& s ) const
   {
     return ( ( team == s.team ) && ( colour == s.colour ) && ( handicap == s.handicap ) && ( side == s.side ) && ( sync == s.sync ) && ( spectator == s.spectator ) && ( ready == s.ready ) && ( owner == s.owner ) && ( aishortname == s.aishortname ) && ( isfromdemo == s.isfromdemo ) && ( aitype == s.aitype ) );
