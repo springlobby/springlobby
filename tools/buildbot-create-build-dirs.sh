e8eebee9eb1aeb387449790e8774bfd0e8a14059
Rene Milk <koshi@springlobby.info>
2009-06-02 17:53:04 +0200

    new gcc4.4 step

--- tools/buildbot-create-build-dirs.sh
+++ tools/buildbot-create-build-dirs.sh
@@ -36,7 +36,7 @@ create-build-dir ()
 	(
 	cd $1
 	if [ ! -f Makefile ] ; then
-		../configure CXXFLAGS="-O0" --config-cache $2
+		../configure  --config-cache $2
 		echo done
 	else
 		# regenerate Makefile with current config options
@@ -48,4 +48,5 @@ create-build-dir ()
 
 create-build-dir build "--with-wx-config=/var/lib/buildbot/lib/linux/wx/2.8/bin/wx-config --with-opt-level=0"
 create-build-dir build-mingw-wx28 "--build=i686-pc-linux-gnu --host=i586-mingw32msvc --with-wx-config=/var/lib/buildbot/lib/mingw/wx/2.8/bin/wx-config  --with-boost-prefix=/var/lib/buildbot/lib/mingw/include/  --with-sdl-config=/var/lib/buildbot/lib/mingw/bin/sdl-config"
-create-build-dir-cmake build-cmake
+create-build-dir-cmake build-cmake "-DCMAKE_CXX_FLAGS:STRING=-O0"
+create-build-dir-cmake build-gcc4.4 "-DCMAKE_CXX_COMPILER:STRING=g++-4.4 -DCMAKE_CXX_FLAGS:STRING=-O0"
