1bc8e6f80bcf26c362939bb1ca3f0d60772cbe10
Andrea Piras <braindamage@springlobby.info>
2009-06-06 03:56:13 +0200

    don't disable relayhost functionality if stat report is off

--- src/tasserver.cpp
+++ src/tasserver.cpp
@@ -679,8 +679,8 @@ void TASServer::ExecuteCommand( const wxString& cmd, const wxString& inparams, i
 					wxString reportstring = _T("stats.report ") + version + _T(" ") + wxversion + _T(" ") + os + aux;
 					if ( UserExists( _T("insanebot") ) ) SayPrivate( _T("insanebot"), reportstring );
 					if ( UserExists( _T("SL_bot") ) ) SayPrivate( _T("SL_bot"), reportstring );
-					if ( UserExists( _T("RelayHostManagerList") ) ) SayPrivate( _T("RelayHostManagerList"), _T("!listmanagers") );
 				}
+				if ( UserExists( _T("RelayHostManagerList") ) ) SayPrivate( _T("RelayHostManagerList"), _T("!listmanagers") );
         m_se->OnLoginInfoComplete();
     }
     else if ( cmd == _T("REMOVEUSER") )
