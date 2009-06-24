5f7b64cb800e63ba94ca0c547e3c6d21c4dc1e11
Andrea Piras <braindamage@springlobby.info>
2009-06-24 17:47:28 +0200

    update ready status checkbox when required

--- src/battleroomtab.cpp
+++ src/battleroomtab.cpp
@@ -501,6 +501,7 @@ void BattleRoomTab::UpdateUser( User& user )
     m_ally_sel->SetSelection( bs.ally );
     m_side_sel->SetSelection( bs.side );
     m_spec_chk->SetValue( bs.spectator );
+    m_ready_chk->SetValue( bs.ready );
     // Enable or disable widgets' sensitivity as appropriate.
     if ( bs.spectator )
     {
