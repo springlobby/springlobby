546dee1f0a44ff772189574c0d3cdb1eacb2b175
Rene Milk <koshi@springlobby.info>
2009-07-01 00:17:31 +0200

    some changes for my new test env

--- cmake/Toolchain-mingw32.cmake
+++ cmake/Toolchain-mingw32.cmake
@@ -2,11 +2,11 @@
 SET(CMAKE_SYSTEM_NAME Windows)
 
 # which compilers to use for C and C++
-SET(CMAKE_C_COMPILER i586-mingw32msvc-gcc)
-SET(CMAKE_CXX_COMPILER i586-mingw32msvc-g++)
-SET( wxWidgets_ROOT_DIR /var/lib/buildbot/lib/mingw/wx/2.8/ )
+SET(CMAKE_C_COMPILER /var/lib/buildbot/lib/mingw_/usr/bin/i386-mingw32msvc-gcc )
+SET(CMAKE_CXX_COMPILER /var/lib/buildbot/lib/mingw_/usr/bin/i386-mingw32msvc-g++)
+SET( wxWidgets_ROOT_DIR /var/lib/buildbot/lib/mingw_/usr/i386-mingw32msvc )
 # here is the target environment located
-SET(CMAKE_FIND_ROOT_PATH  /usr/bin/i586-mingw32msvc /var/lib/buildbot/lib/mingw /var/lib/buildbot/lib/mingw/wx/2.8/include/wx-2.8)
+SET(CMAKE_FIND_ROOT_PATH  /var/lib/buildbot/lib/mingw_/usr/i386-mingw32msvc/bin /var/lib/buildbot/lib/mingw_/usr/i386-mingw32msvc /var/lib/buildbot/lib/mingw_/usr/i386-mingw32msvc/include/wx-2.8)
 
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search 
@@ -14,17 +14,17 @@ SET(CMAKE_FIND_ROOT_PATH  /usr/bin/i586-mingw32msvc /var/lib/buildbot/lib/mingw 
 # SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 # SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 # SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-SET( wxWidgets_CONFIG_EXECUTABLE /var/lib/buildbot/lib/mingw/wx/2.8/bin/wx-config )
+SET( wxWidgets_CONFIG_EXECUTABLE /var/lib/buildbot/lib/mingw_/usr/i386-mingw32msvc/bin/wx-config )
 SET( wxWidgets_LIBRARIES wx_baseu-2.8-i586-mingw32msvc wx_mswu_core-2.8-i586-mingw32msvc wx_baseu_net-2.8-i586-mingw32msvc wx_mswu_adv-2.8-i586-mingw32msvc  wx_mswu_aui-2.8-i586-mingw32msvc wx_mswu_html-2.8-i586-mingw32msvc wx_baseu_xml-2.8-i586-mingw32msvc)
-SET( wxWidgets_INCLUDE_DIRS /var/lib/buildbot/lib/mingw/wx/2.8/include/wx-2.8 /var/lib/buildbot/lib/mingw/wx/2.8/lib/wx/include/i586-mingw32msvc-msw-unicode-release-2.8/)
-SET( wxWidgets_LIB_DIR /var/lib/buildbot/lib/mingw/wx/2.8/lib )
+SET( wxWidgets_INCLUDE_DIRS /var/lib/buildbot/lib/mingw_/usr/i386-mingw32msvc/include/wx-2.8 )
+SET( wxWidgets_LIB_DIR /var/lib/buildbot/lib/mingw_/usr/i386-mingw32msvc/lib )
 SET( wxWidgets_CONFIGURATION mswu )
 SET( CMAKE_VERBOSE_MAKEFILE ON )
 SET( Boost_LIBRARIES boost_thread-mt boost_filesystem-mt boost_date_time-mt )
-SET( boost_LIB_DIR /var/lib/buildbot/lib/mingw/lib )
-SET( boost_INCLUDE_DIR /var/lib/buildbot/lib/mingw/include )
-SET( sdl_INCLUDE_DIR /var/lib/buildbot/lib/mingw/include/SDL )
-SET( wxWidgets_RC_DIR /var/lib/buildbot/lib/mingw/wx/2.8/include/wx-2.8 )
+SET( boost_LIB_DIR /var/lib/buildbot/lib/mingw_/usr/i386-mingw32msvc/lib )
+SET( boost_INCLUDE_DIR /var/lib/buildbot/lib/mingw_/usr/i386-mingw32msvc/include )
+SET( sdl_INCLUDE_DIR /var/lib/buildbot/lib/mingw_/usr/i386-mingw32msvc/include/SDL )
+SET( wxWidgets_RC_DIR /var/lib/buildbot/lib/mingw_/usr/i386-mingw32msvc/include/wx-2.8 )
 
 
 ADD_DEFINITIONS( -DWXUSINGDLL -D__WXMSW__)
