48717eecd7b0a18ba961302c2f8c3050774b6bd4
Rene Milk <koshi@springlobby.info>
2009-07-26 20:18:26 +0200

    enable debug build for windows (release) buildbot, this miraculously fixes curl uploading

--- tools/buildbot-create-build-dirs.sh
+++ tools/buildbot-create-build-dirs.sh
@@ -47,7 +47,10 @@ create-build-dir ()
 }
 
 create-build-dir build "--with-wx-config=/var/lib/buildbot/lib/linux/wx/2.8/bin/wx-config --with-opt-level=0"
-create-build-dir build-mingw-wx28 "--build=i686-pc-linux-gnu --host=i586-mingw32msvc --with-wx-config=/var/lib/buildbot/lib/mingw/wx/2.8/bin/wx-config  --with-boost-prefix=/var/lib/buildbot/lib/mingw/include/  --with-sdl-config=/var/lib/buildbot/lib/mingw/bin/sdl-config --with-opt-level=2"
+
+#DO NOT remove the debug enabling here, curl uploading fails w/o it
+create-build-dir build-mingw-wx28 "--enable-debug --build=i686-pc-linux-gnu --host=i586-mingw32msvc --with-wx-config=/var/lib/buildbot/lib/mingw/wx/2.8/bin/wx-config  --with-boost-prefix=/var/lib/buildbot/lib/mingw/include/  --with-sdl-config=/var/lib/buildbot/lib/mingw/bin/sdl-config --with-opt-level=2"
+
 if [ -e /usr/bin/g++-4.4 ] ; then
 	create-build-dir-cmake build-cmake "-DCMAKE_CXX_COMPILER:STRING=g++-4.4 -DCMAKE_CXX_FLAGS:STRING=-O0"
 else
