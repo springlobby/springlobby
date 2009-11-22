d37aa2459ee802aeee9fda2b82e9e07cd160b31d
Rene Milk <koshi@springlobby.info>
2009-11-22 20:32:48 +0100

    getting a redable response

--- src/utils/downloader.cpp
+++ src/utils/downloader.cpp
@@ -17,21 +17,21 @@
 
 const wxString s_soap_service_url = _T("http://planet-wars.eu/PlasmaServer/Service.asmx?op=DownloadFile");
 
-const wxString s_soap_querytemplate = _T("<?xml version=\"1.0\" encoding=\"utf-8\"?>" \
-"<soap12:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap12=\"http://www.w3.org/2003/05/soap-envelope\">"\
-  "<soap12:Body>"\
-    "<DownloadFile xmlns=\"http://planet-wars.eu/PlasmaServer/\">"\
-      "<internalName>REALNAME</internalName>"\
-    "</DownloadFile>"\
-  "</soap12:Body>"\
-"</soap12:Envelope>");
+const wxString s_soap_querytemplate = _T("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" \
+"<soap12:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap12=\"http://www.w3.org/2003/05/soap-envelope\">\n"\
+"   <soap12:Body>\n"\
+"       <DownloadFile xmlns=\"http://planet-wars.eu/PlasmaServer/\">\n"\
+"           <internalName>REALNAME</internalName>\n"\
+"       </DownloadFile>\n"\
+"   </soap12:Body>\n"\
+"</soap12:Envelope>\0");
 
 wxArrayString getDownloadLinks( const wxString& name ) {
     wxSocketClient * socket = new wxSocketClient();
     wxString data = s_soap_querytemplate;
     data.Replace( _T("REALNAME") , name );
     wxString host = _T("planet-wars.eu");
-    wxString path = _T("/PlasmaServer/Service.asmx?op=DownloadFile");//can be php or any other file
+    wxString path = _T("/PlasmaServer/Service.asmx");//can be php or any other file
 
 
     //Set up header
@@ -40,35 +40,36 @@ wxArrayString getDownloadLinks( const wxString& name ) {
     //POST
     header += _T("POST ");
     header += path;
-    header += _T(" HTTP/1.0\n");
+    header += _T(" HTTP/1.1\n");
 
     //Write host website name
     header += _T("Host: ");
     header += host;
     header += _T("\n");
 
-    //Write user agent
-    header += _T("User-Agent: HTTPTool/1.0\n");
+//    //Write user agent
+//    header += _T("User-Agent: HTTPTool/1.0\n");
+
+    //Write content type
+    header += _T("Content-Type: application/soap+xml; charset=utf-8\n");
 
     //Write POST content length
     header += _T("Content-Length: ");
     header += wxString::Format(_T("%d"), data.Len());
-    header += _T("\n");
-
-    //Write content type
-    header += _T("Content-Type: application/x-www-form-urlencoded\n");
-    header += _T("\n");
+    header += _T("\n\n\n");
 
     //Print on screen to make sure it looks right
     wxMessageBox(header);
+    wxMessageBox(data);
 
     //Connect to host
     wxIPV4address * address = new wxIPV4address();
     address->Hostname(host);
     address->Service(80);
 
-    if (socket->Connect(*address))
-            wxMessageBox(_T("connected"));
+    socket->Connect(*address);
+//    if (socket->Connect(*address))
+//            wxMessageBox(_T("connected"));
 
     //Write header
     socket->Write(header.c_str(),header.Len());
@@ -80,14 +81,14 @@ wxArrayString getDownloadLinks( const wxString& name ) {
 
 
     //Get Response
-    wxString buf;
-    socket->Read(buf.GetWriteBuf(1000),1000);
-    buf.UngetWriteBuf();
+    char buf[2049];
+    socket->Read(buf,2049);
 
-    //Trim response to what was read from stream
-    buf = buf.SubString(0,socket->LastCount()-1);
+
+    buf[socket->LastCount()] = '\0';
+
 //    buf = wxString( buf, wxConvISO8859_1 );
-    buf = wxString::  From8BitData( STD_STRING(buf).c_str() );
-    wxMessageBox(wxString::Format(_T("Read %d bytes: %s"),socket->LastCount(),buf.c_str()));
+    wxString wxbuf = wxString::  FromAscii( buf );
+    wxMessageBox(wxString::Format(_T("Read %d bytes: %s"),socket->LastCount(),wxbuf.c_str()));
     return wxArrayString();
 }
