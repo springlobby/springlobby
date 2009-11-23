8a4e1ec9edd560360d8dee8de16fbbfbed413240
Rene Milk <koshi@springlobby.info>
2009-11-23 23:14:27 +0100

    enable cli arg for custom settings file

--- src/springlobbyapp.cpp
+++ src/springlobbyapp.cpp
@@ -286,7 +286,7 @@ void SpringLobbyApp::OnInitCmdLine(wxCmdLineParser& parser)
     #ifdef __WXMSW__
         { wxCMD_LINE_SWITCH, STR("u"), STR("update"),  _("only run update, quit immediately afterwards"), wxCMD_LINE_VAL_NONE, wxCMD_LINE_PARAM_OPTIONAL },
     #endif
-        //{ wxCMD_LINE_OPTION, STR("c"), STR("config-file"),  _("override default choice for config-file"), wxCMD_LINE_VAL_STRING, wxCMD_LINE_PARAM_OPTIONAL | wxCMD_LINE_NEEDS_SEPARATOR },
+        { wxCMD_LINE_OPTION, STR("c"), STR("config-file"),  _("override default choice for config-file"), wxCMD_LINE_VAL_STRING, wxCMD_LINE_PARAM_OPTIONAL | wxCMD_LINE_NEEDS_SEPARATOR },
         { wxCMD_LINE_OPTION, STR("l"), STR("log-verbosity"),  _("overrides default logging verbosity, can be:\n                                0: no log\n                                1: critical errors\n                                2: errors\n                                3: warnings (default)\n                                4: messages\n                                5: function trace"), wxCMD_LINE_VAL_NUMBER, wxCMD_LINE_PARAM_OPTIONAL },
         { wxCMD_LINE_OPTION, STR("c"), STR("customize"),  _("Load lobby customizations from game archive. Expects the shortname."), wxCMD_LINE_VAL_STRING, wxCMD_LINE_PARAM_OPTIONAL },
         { wxCMD_LINE_SWITCH, STR("s"), STR("simple"),  _("Start with the simple interface."), wxCMD_LINE_VAL_NONE, wxCMD_LINE_PARAM_OPTIONAL },
@@ -310,7 +310,18 @@ bool SpringLobbyApp::OnCmdLineParsed(wxCmdLineParser& parser)
         m_crash_handle_disable = parser.Found(_T("no-crash-handler"));
         m_start_simple_interface = parser.Found(_T("simple"));
 
-//        Settings::m_user_defined_config = parser.Found( _T("config-file"), &Settings::m_user_defined_config_path );
+        Settings::m_user_defined_config = parser.Found( _T("config-file"), &Settings::m_user_defined_config_path );
+        if ( Settings::m_user_defined_config ) {
+             wxFileName fn ( Settings::m_user_defined_config_path );
+             if ( ! fn.IsAbsolute() ) {
+                 wxLogError ( _T("path for parameter \"config-file\" must be absolute") );
+                 return false;
+             }
+             if ( ! fn.IsFileWritable() ) {
+                 wxLogError ( _T("path for parameter \"config-file\" must be writeable") );
+                 return false;
+             }
+        }
 
         if ( !parser.Found(_T("log-verbosity"), &m_log_verbosity ) )
             m_log_verbosity = m_log_window_show ? 3 : 5;
