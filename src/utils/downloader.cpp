8f2cd91d5b92a842c93257373d6b260aa1326be8
Rene Milk <koshi@springlobby.info>
2009-11-24 00:30:54 +0100

    getting a good response

--- src/utils/downloader.cpp
+++ src/utils/downloader.cpp
@@ -26,6 +26,24 @@ const wxString s_soap_querytemplate = _T("<?xml version=\"1.0\" encoding=\"utf-8
 "   </soap12:Body>\n"\
 "</soap12:Envelope>\0");
 
+//const wxString alt_post = _T("POST http://planet-wars.eu/PlasmaServer/Service.asmx HTTP/1.1"\
+//"Accept-Encoding: gzip,deflate
+//"Content-Type: text/xml;charset=UTF-8
+//"SOAPAction: "http://planet-wars.eu/PlasmaServer/DownloadFile"
+//|User-Agent: Jakarta Commons-HttpClient/3.1
+//Host: planet-wars.eu
+//Content-Length: 351
+//
+//<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:plas="http://planet-wars.eu/PlasmaServer/">
+//   <soapenv:Header/>
+//   <soapenv:Body>
+//      <plas:DownloadFile>
+//         <!--Optional:-->
+//         <plas:internalName>Small divide</plas:internalName>
+//      </plas:DownloadFile>
+//   </soapenv:Body>
+//</soapenv:Envelope>
+
 wxArrayString getDownloadLinks( const wxString& name ) {
     wxSocketClient * socket = new wxSocketClient();
     wxString data = s_soap_querytemplate;
@@ -38,25 +56,29 @@ wxArrayString getDownloadLinks( const wxString& name ) {
     wxString header = _T("");
 
     //POST
-    header += _T("POST ");
-    header += path;
+    header += _T("POST http://planet-wars.eu/PlasmaServer/Service.asmx");
+//    header += path;
     header += _T(" HTTP/1.1\n");
+    header += _T("Accept-Encoding: *\n");
 
     //Write host website name
-    header += _T("Host: ");
-    header += host;
-    header += _T("\n");
 
 //    //Write user agent
 //    header += _T("User-Agent: HTTPTool/1.0\n");
 
     //Write content type
-    header += _T("Content-Type: application/soap+xml; charset=utf-8\n");
+    header += _T("Content-Type: text/xml;charset=UTF-8\n");
+    header += _T("SOAPAction: \"http://planet-wars.eu/PlasmaServer/DownloadFile\"\n");
+    header += _T("User-Agent: Jakarta Commons-HttpClient/3.1\n");
+
+    header += _T("Host: ");
+    header += host;
+    header += _T("\n");
 
     //Write POST content length
     header += _T("Content-Length: ");
     header += wxString::Format(_T("%d"), data.Len());
-    header += _T("\n\n\n");
+    header += _T("\n\n");
 
     //Print on screen to make sure it looks right
     wxMessageBox(header);
@@ -72,17 +94,17 @@ wxArrayString getDownloadLinks( const wxString& name ) {
 //            wxMessageBox(_T("connected"));
 
     //Write header
-    socket->Write(header.c_str(),header.Len());
+    socket->Write(header.mb_str(),header.Len());
     wxMessageBox(wxString::Format(_T("Wrote %d out of %d bytes"),socket->LastCount(),header.Len()));
 
     //Write data
-    socket->Write(data.c_str(),data.Len());
+    socket->Write(data.mb_str(),data.Len());
     wxMessageBox(wxString::Format(_T("Wrote %d out of %d bytes"),socket->LastCount(),data.Len()));
 
 
     //Get Response
-    char buf[2049];
-    socket->Read(buf,2049);
+    char buf[4097];
+    socket->Read(buf,4097);
 
 
     buf[socket->LastCount()] = '\0';
