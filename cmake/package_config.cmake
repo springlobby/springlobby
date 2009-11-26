91d4d82f01ff22047cd5f00df630b3e13180eef8
Rene Milk <koshi@springlobby.info>
2009-11-26 23:38:14 +0100

    better rev handling

--- cmake/package_config.cmake
+++ cmake/package_config.cmake
@@ -1,8 +1,10 @@
 # get revision and put into config.h
-# EXECUTE_PROCESS(COMMAND ${SpringLobby_SOURCE_DIR}/tools/get-revision.sh
-#         OUTPUT_VARIABLE SPRINGLOBBY_REV
-#         RESULT_VARIABLE GIT_ERROR
-#         OUTPUT_STRIP_TRAILING_WHITESPACE)
+IF ( NOT $SPRINGLOBBY_REV )
+EXECUTE_PROCESS(COMMAND ${SpringLobby_SOURCE_DIR}/tools/get-revision.sh
+        OUTPUT_VARIABLE SPRINGLOBBY_REV
+        RESULT_VARIABLE GIT_ERROR
+        OUTPUT_STRIP_TRAILING_WHITESPACE)
+ENDIF ( NOT $SPRINGLOBBY_REV )
 
 #don't write when git errored out resulting in unset version (ie when compiling from tarball)
 IF ( NOT GIT_ERROR)
