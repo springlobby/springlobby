635af990fa46d910ea1ee16366585c3f5365013c
Rene Milk <koshi@springlobby.info>
2009-11-27 20:07:05 +0100

    print ally num too(to be removed later again)

--- src/battleroomtab.cpp
+++ src/battleroomtab.cpp
@@ -418,9 +418,11 @@ void BattleRoomTab::PrintAllySetup()
 	if ( m_battle )
 	{
 		std::map<int, int> allysizes = m_battle->GetAllySizes();
-		for ( std::map<int, int>::iterator itor = allysizes.begin(); itor != allysizes.end(); itor++ )
+		for ( std::map<int, int>::const_iterator itor = allysizes.begin(); itor != allysizes.end(); ++itor )
 		{
-			if ( itor != allysizes.begin() ) setupstring += _T("v");
+			if ( itor != allysizes.begin() )
+                setupstring += _T("v");
+			setupstring += wxString::Format( _T("(%d) ") , itor->first );
 			setupstring += TowxString( itor->second );
 		}
 	}
