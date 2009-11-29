32818bffc1d55a01ef49f20a1015ee62fb6a2800
Rene Milk <koshi@springlobby.info>
2009-11-29 16:32:29 +0100

    buildsystem fixes for 4.4

--- cmake/Toolchain-mingw32.cmake
+++ cmake/Toolchain-mingw32.cmake
@@ -15,7 +15,14 @@ SET(CMAKE_FIND_ROOT_PATH  /usr/bin/i586-mingw32msvc /var/lib/buildbot/lib/mingw 
 # SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 # SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 SET( wxWidgets_CONFIG_EXECUTABLE /var/lib/buildbot/lib/mingw/wx/2.8/bin/wx-config )
-SET( wxWidgets_LIBRARIES wx_baseu-2.8-i586-mingw32msvc wx_mswu_core-2.8-i586-mingw32msvc wx_baseu_net-2.8-i586-mingw32msvc wx_mswu_adv-2.8-i586-mingw32msvc  wx_mswu_aui-2.8-i586-mingw32msvc wx_mswu_html-2.8-i586-mingw32msvc wx_baseu_xml-2.8-i586-mingw32msvc)
+SET( wxWidgets_LIBRARIES
+	wx_baseu-2.8-i586-mingw32msvc
+	wx_mswu_core-2.8-i586-mingw32msvc
+	wx_baseu_net-2.8-i586-mingw32msvc
+	wx_mswu_adv-2.8-i586-mingw32msvc
+	wx_mswu_aui-2.8-i586-mingw32msvc
+	wx_mswu_html-2.8-i586-mingw32msvc
+	wx_baseu_xml-2.8-i586-mingw32msvc)
 SET( wxWidgets_INCLUDE_DIRS /var/lib/buildbot/lib/mingw/wx/2.8/include/wx-2.8 /var/lib/buildbot/lib/mingw/wx/2.8/lib/wx/include/i586-mingw32msvc-msw-unicode-release-2.8/)
 SET( wxWidgets_LIB_DIR /var/lib/buildbot/lib/mingw/wx/2.8/lib )
 SET( wxWidgets_CONFIGURATION mswu )
--- tools/buildbot-create-build-dirs.sh
+++ tools/buildbot-create-build-dirs.sh
@@ -48,7 +48,7 @@ create-build-dir ()
 
 #create-build-dir build "--with-wx-config=/var/lib/buildbot/lib/linux/wx/2.8/bin/wx-config --with-opt-level=0"
 create-build-dir-cmake build-cmake "-DCMAKE_CXX_FLAGS:STRING=-O0 -DSPRINGLOBBY_REV:STRING=${1}"
-create-build-dir-cmake build-mingw-wx28 "-DCMAKE_TOOLCHAIN_FILE:STRING=../cmake/Toolchain-mingw32.cmake \
+create-build-dir-cmake build-mingw-wx28 "-DCMAKE_TOOLCHAIN_FILE:STRING=../cmake/Toolchain-mingw32_new.cmake \
 	-DSPRINGLOBBY_REV:STRING=${1} -DCMAKE_INSTALL_PREFIX:STRING=${PWD}/build-mingw-wx28 \
 	-DLOCALE_INSTALL_DIR:STRING=${PWD}/build-mingw-wx28/locale -DCMAKE_BUILD_TYPE:STRING=RELEASE"
 
