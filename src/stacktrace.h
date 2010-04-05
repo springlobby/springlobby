da2e88e7de219db16a204fa8f356a02c7724e79a
Rene Milk <koshi@springlobby.info>
2010-04-05 16:41:33 +0200

    #ifdef cleanup for crash report header

--- src/stacktrace.h
+++ src/stacktrace.h
@@ -3,44 +3,43 @@
 
 #if defined(ENABLE_DEBUG_REPORT)
 
-#if wxUSE_STACKWALKER
-#include <wx/string.h>
-#include <wx/stackwalk.h>
-
-
-class StackTrace : public wxStackWalker
-{
-  public:
-    StackTrace() {}
-    ~StackTrace() {}
-
-    wxString GetStackTrace() { return StackTraceString; }
-
-  private:
-
-    void OnStackFrame( const wxStackFrame& frame );
-
-    wxString StackTraceString;
-    wxString PartToHash;
-
-};
+#ifdef __WXMSW__
+	#include <windows.h>
+	#ifdef __cplusplus
+		extern "C" {
+	#endif
+			 LONG WINAPI TopLevelExceptionFilter(PEXCEPTION_POINTERS pExceptionInfo, const char* report_filename);
+	#ifdef __cplusplus
+		}
+	#endif
 #else
-    #ifdef __WXMSW__
-//		namespace StackTrace {
-			#include <windows.h>
-//			static std::string report_filename;
-//			long Stacktrace(LPEXCEPTION_POINTERS e);
-		#ifdef __cplusplus
-			extern "C" {
-		#endif
-				 LONG WINAPI TopLevelExceptionFilter(PEXCEPTION_POINTERS pExceptionInfo, const char* report_filename);
-		#ifdef __cplusplus
-			}
-		#endif
+
+	#include <wx/string.h>
+	#include <wx/stackwalk.h>
+
+	#if wxUSE_STACKWALKER
+
+
+	class StackTrace : public wxStackWalker
+	{
+	  public:
+		StackTrace() {}
+		~StackTrace() {}
+
+		wxString GetStackTrace() { return StackTraceString; }
+
+	  private:
+
+		void OnStackFrame( const wxStackFrame& frame );
+
+		wxString StackTraceString;
+		wxString PartToHash;
+
+	};
 	#else
 		#error "No usable stacktrace config, set ENABLE_DEBUG_REPORT=OFF in cmake"
-    #endif
-#endif //#if wxUSE_STACKWALKER
+	#endif //#if wxUSE_STACKWALKER
+#endif // __WXMSW__
 
 #endif //#if defined(ENABLE_DEBUG_REPORT)
 
