cc0850fe187127003ba988d0150fbbd39fabc2d8
Rene Milk <koshi@springlobby.info>
2010-04-17 23:11:37 +0200

    explicitly set wx-config var build dir creation

--- tools/buildbot-create-build-dirs.sh
+++ tools/buildbot-create-build-dirs.sh
@@ -21,7 +21,7 @@ create-build-dir-cmake ()
 	)
 }
 
-create-build-dir-cmake build-cmake "-DCMAKE_CXX_FLAGS:STRING=-O0 -DSPRINGLOBBY_REV:STRING=${1}"
+create-build-dir-cmake build-cmake "-DCMAKE_CXX_FLAGS:STRING=-O0 -DSPRINGLOBBY_REV:STRING=${1} -DwxWidgets_CONFIG_EXECUTABLE:STRING=/usr/bin/wx-config"
 create-build-dir-cmake build-mingw-wx28 "-DCMAKE_TOOLCHAIN_FILE:STRING=../cmake/Toolchain-mingw32_new.cmake \
 	-DOPTION_SOUND:BOOL=OFF -DENABLE_DEBUG_REPORT=ON -DCMAKE_EXE_LINKER_FLAGS:STRING=-L/opt/mingw32/lib \
 	-DSPRINGLOBBY_REV:STRING=${1} -DCMAKE_INSTALL_PREFIX:STRING=${PWD}/build-mingw-wx28 -DAUX_VERSION:STRING=msw \
