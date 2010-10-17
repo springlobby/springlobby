6ffe123960eeafd476d002b866fa7d9ec8acf806
Rene Milk <rene.milk@uni-muenster.de>
2010-10-17 18:14:43 +0200

    fix std::exception override again

--- src/settings++/hotkeys/HotkeyException.h
+++ src/settings++/hotkeys/HotkeyException.h
@@ -10,9 +10,7 @@ class HotkeyException : public std::exception
 {
 public:
 	HotkeyException( const wxString& message ) : m_message( message ) {}
-#ifndef __WXMSW__
 	virtual ~HotkeyException() throw() {}
-#endif
 
 	const char* what() const throw() { return "Use HotkeyException::getMessage() instead"; };
 	const wxString& getMessage() const
