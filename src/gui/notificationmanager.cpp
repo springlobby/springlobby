27921f1d70e3c3b0a8afac0bb6bc4e58d81284f3
Rene Milk <rene.milk@uni-muenster.de>
2010-10-22 22:02:38 +0200

    disable snarl interface

--- src/gui/notificationmanager.cpp
+++ src/gui/notificationmanager.cpp
@@ -13,9 +13,9 @@
 #ifdef HAVE_LIBNOTIFY
 	#include "libnotify.h"
 	typedef LibnotifyNotification NotificationWrapperType;
-#elif defined(__WXMSW__)
-	#include "snarlnotification.h"
-	typedef SnarlNotification NotificationWrapperType;
+//#elif defined(__WXMSW__) //this is currently completely screwed
+//	#include "snarlnotification.h"
+//	typedef SnarlNotification NotificationWrapperType;
 #else
 	#include "toasternotification.h"
 	typedef ToasterNotification NotificationWrapperType;
