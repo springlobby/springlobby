32cf283ab64ee8bb1a4281a47c82526d572c419f
verybadsoldier <vbs@springrts.de>
2010-11-06 18:35:38 +0100

    wxKeyMonitorTextCtrl moved to own file cause of loop include problems

--- src/wxkeybinder/keybinder.cpp
+++ src/wxkeybinder/keybinder.cpp
@@ -72,19 +72,6 @@ BEGIN_EVENT_TABLE(wxBinderEvtHandler, wxEvtHandler)
 END_EVENT_TABLE()
 
 
-
-// event table for wxKeyMonitorTextCtrl
-IMPLEMENT_CLASS(wxKeyMonitorTextCtrl, wxTextCtrl)
-BEGIN_EVENT_TABLE(wxKeyMonitorTextCtrl, wxTextCtrl)
-
-    // keydown events
-    EVT_KEY_DOWN(wxKeyMonitorTextCtrl::OnKey)
-    EVT_KEY_UP(wxKeyMonitorTextCtrl::OnKey)
-
-END_EVENT_TABLE()
-
-
-
 // event table for wxKeyConfigPanel
 IMPLEMENT_CLASS(wxKeyConfigPanel, wxPanel)
 BEGIN_EVENT_TABLE(wxKeyConfigPanel, wxPanel)
@@ -1285,40 +1272,6 @@ bool wxKeyProfileArray::Load(wxConfigBase *p, const wxString &key)
 
     return TRUE;
 }
-
-
-
-
-// ----------------------------------------------------------------------------
-// wxKeyMonitorTextCtrl
-// ----------------------------------------------------------------------------
-
-void wxKeyMonitorTextCtrl::OnKey(wxKeyEvent &event)
-{
-    // backspace cannot be used as shortcut key...
-#ifndef wxKEYBINDER_ALLOW_BACKSPACE
-	if (event.GetKeyCode() == WXK_BACK) {
-
-        // this text ctrl contains something and the user pressed backspace...
-        // we must delete the keypress...
-        Clear();
-        return;
-    }
-#endif
-
-    if (event.GetEventType() == wxEVT_KEY_DOWN ||
-        (event.GetEventType() == wxEVT_KEY_UP && !IsValidKeyComb())) {
-
-        // the user pressed some key combination which must be displayed
-        // in this text control.... or he has just stopped pressing a
-        // modifier key like shift, ctrl or alt without adding any
-        // other alphanumeric char, thus generating an invalid keystroke
-        // which must be cleared out...
-        SetValue(wxKeyBind::GetKeyStrokeString(event));
-        SetInsertionPointEnd();
-    }
-}
-
 
 
 
--- src/wxkeybinder/keybinder.h
+++ src/wxkeybinder/keybinder.h
@@ -1259,47 +1259,6 @@ public:
 
 
 
-//! A special text control where the user keypresses are displayed
-//! as the contained text.
-//! This class is used by wxKeyConfigPanel class to allow the user
-//! to input a shortcut key which can be associated with a wxCmd.
-//!
-//! Example: the user presses CTRL+SHIFT+A inside this window;
-//!          the wxKeyMonitorTextCtrl displays exactly the string
-//!          "Ctrl+Shift+A"
-//!
-class wxKeyMonitorTextCtrl : public wxTextCtrl
-{
-public:
-    wxKeyMonitorTextCtrl(
-        wxWindow* parent,
-        wxWindowID id,
-        const wxString& value = wxEmptyString,
-        const wxPoint& pos = wxDefaultPosition,
-        const wxSize& size = wxDefaultSize,
-        long style = wxTE_PROCESS_ENTER,
-        const wxValidator& validator = wxDefaultValidator,
-        const wxString& name = wxTextCtrlNameStr) :
-        wxTextCtrl(parent, id, value, pos, size, style, validator, name) {}
-
-    virtual ~wxKeyMonitorTextCtrl() {}
-
-public:
-
-    //! Handles the keypresses and changes accordingly
-    //! the text displayed in text ctrl.
-    void OnKey(wxKeyEvent &);
-
-    //! Returns TRUE if this window is containing a valid key combination.
-    bool IsValidKeyComb() const {
-        return !GetValue().IsEmpty(); //we allow '+' (&& GetValue().Last() != '+';)
-    }
-
-private:
-    DECLARE_CLASS(wxKeyMonitorTextCtrl)
-    DECLARE_EVENT_TABLE()
-};
-
 
 
 
