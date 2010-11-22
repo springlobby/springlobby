7013561a88c4ee2c64ce8667ce4596272fc2a640
Rene Milk <rene.milk@uni-muenster.de>
2010-11-22 01:48:20 +0100

    this fixes #1460

--- src/utils/downloader.cpp
+++ src/utils/downloader.cpp
@@ -33,7 +33,7 @@
 #include <curl/easy.h>
 #include "curlhelper.h"
 
-const wxString s_soap_service_url = _T("http://planet-wars.eu/PlasmaServer/Service.asmx?op=DownloadFile");
+const wxString s_soap_service_url = _T("http://zero-k.info/ContentService.asmx?op=DownloadFile");
 
 const wxString s_soap_querytemplate = _T("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" \
 "<soap12:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap12=\"http://www.w3.org/2003/05/soap-envelope\">\n"\
