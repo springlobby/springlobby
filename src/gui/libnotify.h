b05d851e043e2cc13bf27e8bf0abdabc36a02400
Rene Milk <rene.milk@uni-muenster.de>
2011-03-17 02:24:40 +0100

    fix libt builds for versions 0.7 onwards

--- src/gui/libnotify.h
+++ src/gui/libnotify.h
@@ -21,7 +21,11 @@ void LibnotifyNotification::Show(const wxBitmap& icon, const size_t /*pos*/, con
 {
 	NotifyNotification *n;
 	notify_init("Test");
-	n = notify_notification_new ( GetAppName().mb_str(),data.second.mb_str(), NULL, NULL);
+	#if defined(LIBNOTIFY_MINOR_VERSION) && LIBNOTIFY_MINOR_VERSION  > 6
+		n = notify_notification_new ( GetAppName().mb_str(),data.second.mb_str(), NULL );
+	#else
+		n = notify_notification_new ( GetAppName().mb_str(),data.second.mb_str(), NULL, NULL );
+	#endif
 	notify_notification_set_timeout (n, sett().GetNotificationPopupDisplayTime()*1000);
 
 	notify_notification_set_icon_from_pixbuf(n,icon.GetPixbuf() );
