0351f9112e8bca9136a9b3df38d1e7e104ce0634
Rene Milk <rene.milk@uni-muenster.de>
2011-03-29 00:40:04 +0200

    sasi: trying another approach to usync loading

--- src/settings.cpp
+++ src/settings.cpp
@@ -913,7 +913,7 @@ wxString Settings::GetCurrentUsedSpringBinary()
 wxString Settings::GetCurrentUsedUnitSync()
 {
 	if ( IsPortableMode() ) return GetCurrentUsedDataDir() + wxFileName::GetPathSeparator() + _T( "unitsync" ) + GetLibExtension();
-#if defined(__WXMSW__) && !defined(SL_QT_MODE)
+#if defined(__WXMSW__)
 	else if ( GetSearchSpringOnlyInSLPath() ) return GetExecutableFolder() + wxFileName::GetPathSeparator() + _T( "unitsync" ) + GetLibExtension();
 #endif
 	else return GetUnitSync( GetCurrentUsedSpringIndex() );
@@ -922,7 +922,7 @@ wxString Settings::GetCurrentUsedUnitSync()
 wxString Settings::GetCurrentUsedUikeys()
 {
 	if ( IsPortableMode() ) return GetCurrentUsedDataDir() + wxFileName::GetPathSeparator() + _T( "uikeys.txt" );
-#if defined(__WXMSW__) && !defined(SL_QT_MODE)
+#if defined(__WXMSW__)
 	else if ( GetSearchSpringOnlyInSLPath() ) return GetExecutableFolder() + wxFileName::GetPathSeparator() + _T( "uikeys.txt" );
 #endif
 	else return GetUikeys( GetCurrentUsedSpringIndex() );
