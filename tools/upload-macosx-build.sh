87f46159615f20388a846fee130d05a8f1bbdeac
Rene Milk <rene.milk@uni-muenster.de>
2011-09-25 13:03:45 +0200

    fix rsync port in macupload script

--- tools/upload-macosx-build.sh
+++ tools/upload-macosx-build.sh
@@ -90,4 +90,4 @@ done
 
 ARCHIVE_NAME=${BUNDLENAME}-MacOSX-10.6-SnowLeopard.zip
 zip -r9 ${OUTPUTDIR}/${ARCHIVE_NAME} ${OUTPUTDIR}/${BUNDLE_NAME}
-rsync -a ${ARCHIVE_NAME} buildbot@springlobby.info:/usr/local/www/springlobby.info/macosx/${builds}/
+rsync --port 50 -a ${ARCHIVE_NAME} buildbot@springlobby.info:/usr/local/www/springlobby.info/macosx/${builds}/
