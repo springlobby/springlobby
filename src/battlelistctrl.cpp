ab0de0879e148dae94577b3dc135771f43a210b3
Rene Milk <rene.milk@uni-muenster.de>
2012-04-21 11:16:37 +0200

    change battle length display from datetime to timespan, fixes #1662

--- src/battlelistctrl.cpp
+++ src/battlelistctrl.cpp
@@ -103,7 +103,8 @@ wxString BattleListCtrl::GetItemText(long item, long column) const
 		case 7: return wxFormat(_T("%d") ) % int(battle.GetSpectators());
 		case 8: return wxFormat(_T("%d") ) % (int(battle.GetNumUsers()) - int(battle.GetSpectators()));
 		case 9: return wxFormat(_T("%d") ) % int(battle.GetMaxPlayers());
-        case 10: return ( wxDateTime(battle.GetBattleRunningTime()).FormatISOTime() );
+        case 10: return ( wxTimeSpan(0/*h*/,0/*m*/,
+                                     battle.GetBattleRunningTime()).Format(_T("%H:%M")) );
     }
 }
 
