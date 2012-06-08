2b03accfc9be934c5ecb9b71eb5575d7ecabe9e4
Rene Milk <rene.milk@uni-muenster.de>
2012-06-09 00:53:40 +0200

    fixup settings standalone compile and older toolchain file

--- cmake/Toolchain-mingw32.cmake
+++ cmake/Toolchain-mingw32.cmake
@@ -1,43 +1,55 @@
 # the name of the target operating system
 SET(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_FIND_LIBRARY_SUFFIXES  ".a" )
 
 # which compilers to use for C and C++
-SET(CMAKE_C_COMPILER i586-mingw32msvc-gcc)
-SET(CMAKE_CXX_COMPILER i586-mingw32msvc-g++)
-SET( wxWidgets_ROOT_DIR /var/lib/buildbot/lib/mingw/wx/2.8/ )
-# here is the target environment located
-SET(CMAKE_FIND_ROOT_PATH  /usr/bin/i586-mingw32msvc /var/lib/buildbot/lib/mingw /var/lib/buildbot/lib/mingw/wx/2.8/include/wx-2.8)
-
-# adjust the default behaviour of the FIND_XXX() commands:
-# search headers and libraries in the target environment, search 
-# programs in the host environment
-# SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-# SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-# SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-SET( wxWidgets_CONFIG_EXECUTABLE /var/lib/buildbot/lib/mingw/wx/2.8/bin/wx-config )
+SET(CMAKE_C_COMPILER /usr/bin/i586-mingw32msvc-gcc)
+SET(CMAKE_CXX_COMPILER /usr/bin/i586-mingw32msvc-g++)
+SET(CMAKE_RC_COMPILER /usr/bin/i586-mingw32msvc-windres)
+SET( MINGWLIBS /opt/mingw32/ CACHE INTERNAL MINGWLIB_DUMMMY_DIR FORCE  )
+SET( BUILD_SHARED_LIBS OFF CACHE INTERNAL MINGWLIB_DUMMMY_DIR FORCE  )
+SET(CMAKE_FIND_ROOT_PATH /opt/mingw32 )
+SET( wxWidgets_CONFIG_EXECUTABLE /opt/mingw32/bin/wx-config )
 SET( wxWidgets_LIBRARIES
-	wx_baseu-2.8-i586-mingw32msvc
-	wx_mswu_core-2.8-i586-mingw32msvc
-	wx_baseu_net-2.8-i586-mingw32msvc
-	wx_mswu_adv-2.8-i586-mingw32msvc
-	wx_mswu_aui-2.8-i586-mingw32msvc
-	wx_mswu_html-2.8-i586-mingw32msvc
-	wx_baseu_xml-2.8-i586-mingw32msvc)
-SET( wxWidgets_INCLUDE_DIRS /var/lib/buildbot/lib/mingw/wx/2.8/include/wx-2.8 /var/lib/buildbot/lib/mingw/wx/2.8/lib/wx/include/i586-mingw32msvc-msw-unicode-release-2.8/)
-SET( wxWidgets_LIB_DIR /var/lib/buildbot/lib/mingw/wx/2.8/lib )
+	wx_baseu_net-2.8-i586-pc-mingw32.dll
+	wx_mswu_core-2.8-i586-pc-mingw32.dll
+	wx_baseu-2.8-i586-pc-mingw32.dll
+	wx_mswu_adv-2.8-i586-pc-mingw32.dll
+	wx_mswu_aui-2.8-i586-pc-mingw32.dll
+	wx_mswu_html-2.8-i586-pc-mingw32.dll
+	wx_mswu_gl-2.8-i586-pc-mingw32.dll
+	wx_baseu_xml-2.8-i586-pc-mingw32.dll
+	wx_mswu_qa-2.8-i586-pc-mingw32.dll
+	wx_mswu_richtext-2.8-i586-pc-mingw32.dll
+	wx_mswu_xrc-2.8-i586-pc-mingw32.dll)
+SET( wxWidgets_INCLUDE_DIRS /opt/mingw32/include/wx-2.8/ /opt/mingw32/lib/wx/include/i586-pc-mingw32-msw-unicode-release-2.8/)
+SET( wxWidgets_LIB_DIR /opt/mingw32/lib )
 SET( wxWidgets_CONFIGURATION mswu )
-SET( CMAKE_VERBOSE_MAKEFILE ON )
-SET( sdl_INCLUDE_DIR /var/lib/buildbot/lib/mingw/include/SDL )
-SET( sdl_LIB_DIR /var/lib/buildbot/lib/mingw/lib )
-SET( libcurl_INCLUDE_DIR /var/lib/buildbot/lib/mingw/include/ )
-SET( libcurl_LIB_DIR /var/lib/buildbot/lib/mingw/lib/ )
-SET( wxWidgets_RC_DIR /var/lib/buildbot/lib/mingw/wx/2.8/include/wx-2.8 )
+SET( CMAKE_VERBOSE_MAKEFILE OFF )
+SET( wxWidgets_RC_DIR /opt/mingw32/include/wx-2.8 )
+#otherwise cmake finds linux lib for win...
+SET( ENV{OPENALDIR} /opt/mingw32/ )
+SET( OPENAL_LIBRARY OpenAL32 )
+SET( OPENAL_INCLUDE_DIR /opt/mingw32/include/AL )
+SET( Boost_LIBRARIES
+	boost_thread-gcc44-mt-1_41
+	boost_filesystem-gcc44-mt-1_41
+	boost_system-gcc44-mt-1_41 )
+SET( boost_LIB_DIR /opt/mingw32/lib )
+SET( boost_INCLUDE_DIR /opt/mingw32/include )
+SET( WINDRES /usr/bin/i586-mingw32msvc-windres )
+link_directories( /opt/mingw32/lib )
+INCLUDE_DIRECTORIES(/opt/mingw32/include  )
+INCLUDE_DIRECTORIES(/opt/mingw32/include/drmingw/include  )
 
-SET( Boost_LIBRARIES libboost_thread-gcc42-mt-d-1_41.a boost_filesystem-gcc42-mt-d-1_41 boost_date_time-gcc42-mt-d-1_41 boost_system-gcc42-mt-d-1_41 )
-SET( boost_LIB_DIR /var/lib/buildbot/lib/mingw/lib )
-SET( boost_INCLUDE_DIR /var/lib/buildbot/lib/mingw/include )
-link_directories( /var/lib/buildbot/lib/mingw/lib )
-INCLUDE_DIRECTORIES(/var/lib/buildbot/lib/mingw/include /var/lib/buildbot/lib/mingw/include/zlib/ )
+SET( PKG_CONFIG_EXECUTABLE /opt/mingw32/bin/pkg-config )
+ADD_DEFINITIONS( -DCURL_STATICLIB -DWXUSINGDLL -D__WXMSW__ -mthreads)
 
-ADD_DEFINITIONS( -DWXUSINGDLL -D__WXMSW__)
-
+SET( LOCALE_INSTALL_DIR "${CMAKE_BINARY_DIR}/locale" CACHE STRING
+	"message catalogs will installed here" FORCE )
+SET( CMAKE_INSTALL_PREFIX "${CMAKE_BINARY_DIR}" CACHE STRING
+	"install prefix" FORCE )
+SET( CURL_CFLAGS "-I/opt/mingw32/include" )
+SET( CURL_STATIC_LIBRARY_DIRS "/opt/mingw32/lib")
+SET( CURL_STATIC_LDFLAGS "-L/opt/mingw32/lib;-lcurl;-lws2_32")
+SET( CURL_INCLUDE_DIR "/opt/mingw32/include")
