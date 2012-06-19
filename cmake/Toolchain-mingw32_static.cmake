02d28b428c7d19e3bf5cd87598d91f500f2087a6
Rene Milk <rene.milk@uni-muenster.de>
2012-06-19 23:44:02 +0200

    trial and error mingw static linkage chain

--- cmake/Toolchain-mingw32_static.cmake
+++ cmake/Toolchain-mingw32_static.cmake
@@ -18,17 +18,21 @@ SET( BUILD_SHARED_LIBS OFF CACHE INTERNAL MINGWLIB_DUMMMY_DIR FORCE  )
 SET(CMAKE_FIND_ROOT_PATH /opt/mingw32 )
 SET( wxWidgets_CONFIG_EXECUTABLE /opt/mingw32/usr/i686-pc-mingw32/bin/wx-config )
 SET( wxWidgets_LIBRARIES
-	libwx_baseu-2.8-i686-pc-mingw32.a
-	libwx_mswu_core-2.8-i686-pc-mingw32.a
-	libwx_mswu_adv-2.8-i686-pc-mingw32.a
-	libwx_mswu_aui-2.8-i686-pc-mingw32.a
-	libwx_mswu_html-2.8-i686-pc-mingw32.a
-	libwx_mswu_gl-2.8-i686-pc-mingw32.a
-	libwx_baseu_xml-2.8-i686-pc-mingw32.a
-	libwx_mswu_qa-2.8-i686-pc-mingw32.a
-	libwx_mswu_richtext-2.8-i686-pc-mingw32.a
-	libwx_mswu_xrc-2.8-i686-pc-mingw32.a
-	png tiff jpeg lzma alure)
+# 	libwx_baseu-2.8-i686-pc-mingw32.a
+# 	libwx_mswu_core-2.8-i686-pc-mingw32.a
+# 	libwx_mswu_adv-2.8-i686-pc-mingw32.a
+# 	libwx_mswu_aui-2.8-i686-pc-mingw32.a
+# 	libwx_mswu_html-2.8-i686-pc-mingw32.a
+# 	libwx_mswu_gl-2.8-i686-pc-mingw32.a
+# 	libwx_baseu_xml-2.8-i686-pc-mingw32.a
+# 	libwx_mswu_qa-2.8-i686-pc-mingw32.a
+# 	libwx_mswu_richtext-2.8-i686-pc-mingw32.a
+# 	libwx_mswu_xrc-2.8-i686-pc-mingw32.a
+libwx_baseu-2.8-i686-pc-mingw32.a      libwx_mswu_adv-2.8-i686-pc-mingw32.a   libwx_mswu_html-2.8-i686-pc-mingw32.a      libwxregexu-2.8-i686-pc-mingw32.a
+libwx_baseu_net-2.8-i686-pc-mingw32.a  libwx_mswu_aui-2.8-i686-pc-mingw32.a   libwx_mswu_qa-2.8-i686-pc-mingw32.a
+libwx_baseu_xml-2.8-i686-pc-mingw32.a  libwx_mswu_core-2.8-i686-pc-mingw32.a  libwx_mswu_richtext-2.8-i686-pc-mingw32.a
+libwx_msw_aui-2.8-i686-pc-mingw32.a    libwx_mswu_gl-2.8-i686-pc-mingw32.a    libwx_mswu_xrc-2.8-i686-pc-mingw32.a
+	 )
 SET( wxWidgets_INCLUDE_DIRS /opt/mingw32/usr/i686-pc-mingw32/include/wx-2.8/ /opt/mingw32/usr/i686-pc-mingw32/lib/wx/include/i586-pc-mingw32-msw-unicode-release-2.8/)
 SET( wxWidgets_LIB_DIR /opt/mingw32/usr/i686-pc-mingw32/lib )
 SET( wxWidgets_CONFIGURATION mswu )
@@ -57,7 +61,14 @@ SET( LOCALE_INSTALL_DIR "${CMAKE_BINARY_DIR}/locale" CACHE STRING
 	"message catalogs will installed here" FORCE )
 SET( CMAKE_INSTALL_PREFIX "${CMAKE_BINARY_DIR}" CACHE STRING
 	"install prefix" FORCE )
-ADD_DEFINITIONS(-mthreads)
+ADD_DEFINITIONS(-mthreads -D_WIN32_WINNT=0x0501)
+LINK_LIBRARIES(
+libwx_baseu-2.8-i686-pc-mingw32.a      libwx_mswu_adv-2.8-i686-pc-mingw32.a   libwx_mswu_html-2.8-i686-pc-mingw32.a      libwxregexu-2.8-i686-pc-mingw32.a
+libwx_baseu_net-2.8-i686-pc-mingw32.a  libwx_mswu_aui-2.8-i686-pc-mingw32.a   libwx_mswu_qa-2.8-i686-pc-mingw32.a
+libwx_baseu_xml-2.8-i686-pc-mingw32.a  libwx_mswu_core-2.8-i686-pc-mingw32.a  libwx_mswu_richtext-2.8-i686-pc-mingw32.a
+libwx_msw_aui-2.8-i686-pc-mingw32.a    libwx_mswu_gl-2.8-i686-pc-mingw32.a    libwx_mswu_xrc-2.8-i686-pc-mingw32.a
+	png tiff jpeg lzma alure xml2 gnutls-openssl ssh2 idn OpenAL32 nettle iconv gcrypt wldap32 gnutls 
+	expat gmp hogweed gpg-error SDL gnutls nettle curl ws2_32 boost_thread-mt intl shell32 setupapi comctl32 OpenAL32 )
 SET( CURL_CFLAGS "-I/opt/mingw32/usr/i686-pc-mingw32/include" )
 # SET( CURL_STATIC_LIBRARY_DIRS "/opt/mingw32/usr/i686-pc-mingw32/lib")
 # SET( CURL_STATIC_LDFLAGS "-L/opt/mingw32/usr/i686-pc-mingw32/lib;-lcurl;-lws2_32")
