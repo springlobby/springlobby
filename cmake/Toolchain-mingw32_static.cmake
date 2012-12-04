42c9b013f490d2794dbd7f29908947ec332e812c
abma <spring@abma.de>
2012-12-04 11:55:20 +0100

    reorder libs to fix linker errors

--- cmake/Toolchain-mingw32_static.cmake
+++ cmake/Toolchain-mingw32_static.cmake
@@ -17,10 +17,16 @@ SET( MINGWLIBS /opt/mingw32/ CACHE INTERNAL MINGWLIB_DUMMMY_DIR FORCE  )
 SET( BUILD_SHARED_LIBS OFF CACHE INTERNAL MINGWLIB_DUMMMY_DIR FORCE  )
 SET( wxWidgets_CONFIG_EXECUTABLE /opt/mingw32/usr/i686-pc-mingw32/bin/wx-config )
 SET( wxWidgets_LIBRARIES
-	wx_baseu-2.8-i686-pc-mingw32.a      wx_mswu_adv-2.8-i686-pc-mingw32.a   wx_mswu_html-2.8-i686-pc-mingw32.a      
-	wxregexu-2.8-i686-pc-mingw32.a		wx_baseu_net-2.8-i686-pc-mingw32.a  wx_mswu_aui-2.8-i686-pc-mingw32.a   
-	wx_mswu_qa-2.8-i686-pc-mingw32.a 	wx_baseu_xml-2.8-i686-pc-mingw32.a  wx_mswu_core-2.8-i686-pc-mingw32.a  
+	wx_mswu_adv-2.8-i686-pc-mingw32.a
+	wx_mswu_html-2.8-i686-pc-mingw32.a
+	wxregexu-2.8-i686-pc-mingw32.a
+	wx_baseu_net-2.8-i686-pc-mingw32.a
+	wx_mswu_aui-2.8-i686-pc-mingw32.a
+	wx_mswu_qa-2.8-i686-pc-mingw32.a
+	wx_baseu_xml-2.8-i686-pc-mingw32.a
+	wx_mswu_core-2.8-i686-pc-mingw32.a
 	wx_mswu_richtext-2.8-i686-pc-mingw32.a
+	wx_baseu-2.8-i686-pc-mingw32.a
 )
 SET( wxWidgets_INCLUDE_DIRS /opt/mingw32/usr/i686-pc-mingw32/include/wx-2.8/ /opt/mingw32/usr/i686-pc-mingw32/lib/wx/include/i586-pc-mingw32-msw-unicode-release-2.8/)
 SET( wxWidgets_LIB_DIR /opt/mingw32/usr/i686-pc-mingw32/lib )
@@ -47,8 +53,8 @@ SET( CMAKE_INSTALL_PREFIX "${CMAKE_BINARY_DIR}" CACHE STRING
 ADD_DEFINITIONS(-D__WXMSW__ -mthreads -D_WIN32_WINNT=0x0501  -DBOOST_THREAD_USE_LIB -DAL_LIBTYPE_STATIC )
 LINK_LIBRARIES(
 	${wxWidgets_LIBRARIES}
-	png tiff jpeg lzma xml2 gnutls-openssl ssh2 idn OpenAL32 nettle iconv gcrypt wldap32 gnutls 
-	expat gmp hogweed gpg-error SDL curl ws2_32 intl shell32 setupapi comctl32 OpenAL32 z winmm
+	png tiff jpeg lzma xml2 gnutls-openssl ssh2 idn OpenAL32 gcrypt wldap32 gnutls nettle
+	expat hogweed gpg-error SDL curl ws2_32 intl iconv shell32 setupapi comctl32 OpenAL32 z winmm gmp
 	)
 SET(CROSS_COMPILING "ON")
 
