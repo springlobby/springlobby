31bf85ec0828655214263d6a3add1af913c29926
Rene Milk <rene.milk@uni-muenster.de>
2013-02-06 10:45:44 +0100

    move new msw compile options to buildbot master config

--- buildbot/create-build-dirs.sh
+++ buildbot/create-build-dirs.sh
@@ -21,8 +21,7 @@ fi
 export CXX=/opt/mingw32/usr/bin/i686-pc-mingw32-g++
 export CC=/opt/mingw32/usr/bin/i686-pc-mingw32-gcc
 
-PARAMS="-DSPRINGLOBBY_REV=${REV} -DCMAKE_FIND_ROOT_PATH_MODE_PROGRAM:STRING=NEVER -DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY:STRING=ONLY -DCMAKE_FIND_ROOT_PATH:PATH=/opt/mingw32/usr/i686-pc-mingw32 -DCMAKE_SYSTEM_NAME:STRING=Windows -DBoost_THREADAPI:STRING=win32 -DCURL_CONFIG:PATH=/opt/mingw32/usr/i686-pc-mingw32/bin/curl-config -DCMAKE_RC_COMPILER:PATH=/opt/mingw32/usr/bin/i686-pc-mingw32-windres"
-#FIXME: should be this:(move to buildbot master config!) PARAMS=$@
+PARAMS="-DSPRINGLOBBY_REV=${REV}" ${@}
 
 echo configuring ${DIR} with $PARAMS
 cd ${DIR}
