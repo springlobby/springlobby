f201092ca1e6eeb4ee09a56c7ded16a9f4e96234
Rene Milk <rene.milk@uni-muenster.de>
2013-02-11 19:46:51 +0100

    set locale install dir via tc file again'

--- cmake/Toolchain-mingw32_static.cmake
+++ cmake/Toolchain-mingw32_static.cmake
@@ -1,1 +1,5 @@
+SET( LOCALE_INSTALL_DIR "${CMAKE_BINARY_DIR}/locale" CACHE STRING
+	"message catalogs will installed here" FORCE )
+SET( CMAKE_INSTALL_PREFIX "${CMAKE_BINARY_DIR}" CACHE STRING
+	"install prefix" FORCE )
 
