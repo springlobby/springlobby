37470299da50791b38aa143c2c5df4914a061a2c
Rene Milk <rene.milk@uni-muenster.de>
2013-04-29 00:43:59 +0200

    mostly fixes linker errors in msw build

--- src/Helper/slhtmlwindow.cpp
+++ src/Helper/slhtmlwindow.cpp
@@ -24,3 +24,62 @@ void slHtmlWindow::OnLinkClicked( wxHtmlLinkEvent& evt )
     wxString url = evt.GetLinkInfo().GetHref();
     OpenWebBrowser( url );
 }
+
+void slHtmlWindow::SetHTMLWindowTitle(wxString const& title)
+{
+  wxHtmlWindow::SetHTMLWindowTitle(title);
+}
+
+void slHtmlWindow::OnLinkClicked(const wxHtmlLinkInfo& link)
+{
+  wxHtmlWindow::OnLinkClicked(link);
+}
+
+void slHtmlWindow::OnHTMLLinkClicked(const wxHtmlLinkInfo& link)
+{
+  wxHtmlWindow::OnHTMLLinkClicked(link);
+}
+
+wxHtmlOpeningStatus slHtmlWindow::OnHTMLOpeningURL(wxHtmlURLType type,
+                                             const wxString& url,
+                                             wxString *redirect) const
+{
+  return wxHtmlWindow::OnHTMLOpeningURL(type, url, redirect);
+}
+
+wxPoint slHtmlWindow::HTMLCoordsToWindow(wxHtmlCell *cell,
+                                   const wxPoint& pos) const
+{
+  return wxHtmlWindow::HTMLCoordsToWindow(cell, pos);
+}
+
+wxWindow* slHtmlWindow::GetHTMLWindow()
+{
+  return wxHtmlWindow::GetHTMLWindow();
+}
+
+wxColour slHtmlWindow::GetHTMLBackgroundColour() const
+{
+  return wxHtmlWindow::GetHTMLBackgroundColour();
+}
+
+void slHtmlWindow::SetHTMLBackgroundColour(const wxColour& clr)
+{
+  wxHtmlWindow::SetHTMLBackgroundColour(clr);
+}
+
+void slHtmlWindow::SetHTMLBackgroundImage(const wxBitmap& bmpBg)
+{
+  wxHtmlWindow::SetHTMLBackgroundImage(bmpBg);
+}
+
+void slHtmlWindow::SetHTMLStatusText(const wxString& text)
+{
+  wxHtmlWindow::SetHTMLStatusText(text);
+}
+
+wxCursor slHtmlWindow::GetHTMLCursor(HTMLCursor type) const
+{
+  return wxHtmlWindow::GetHTMLCursor(type);
+}
+
