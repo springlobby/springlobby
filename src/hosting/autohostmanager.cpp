2fc0c049fd9f66e95ad758f29530058b5a36b5ef
N0U <petushkov.egor@gmail.com>
2013-06-07 20:56:04 -0800

    Basic inerface for operations with autohost.

---
+++ src/hosting/autohostmanager.cpp
@@ -0,0 +1,156 @@
+#include "autohostmanager.h"
+
+#include "../battle.h"
+
+AutohostHandler::AutohostHandler():m_battle(0)
+{
+}
+
+AutohostHandler::~AutohostHandler()
+{
+
+}
+
+void AutohostHandler::SetBattle(Battle* battle)
+{
+    m_battle=battle;
+}
+
+//========================
+//-------- Springie ------
+//========================
+
+SpringieHandler::SpringieHandler():AutohostHandler()
+{
+
+}
+
+SpringieHandler::~SpringieHandler()
+{
+
+}
+
+void SpringieHandler::Balance()
+{
+    m_battle->Say(_T("!balance"));
+}
+
+void SpringieHandler::SetRandomMap()
+{
+    m_battle->Say(_T("!map"));
+}
+
+void SpringieHandler::SetMap(wxString& map)
+{
+    m_battle->Say(_T("!map")+map);
+}
+
+void SpringieHandler::ClearStartBoxes()
+{
+    m_battle->Say(_T("!clear")); /// will check
+}
+
+void SpringieHandler::AddStartBox(int posx,int posy,int w,int h)
+{
+    m_battle->Say(wxString::Format(wxT("!addbox %i %i %i %i"),posx,posy,w,h));
+}
+
+void SpringieHandler::Notify()
+{
+    m_battle->Say(_T("!notify"));
+}
+//------------------------------
+
+//========================
+//-------- Spads ---------
+//========================
+SpadsHandler::SpadsHandler():AutohostHandler()
+{
+
+}
+
+SpadsHandler::~SpadsHandler()
+{
+
+}
+
+void SpadsHandler::Balance()
+{
+    m_battle->Say(_T("!balance"));
+}
+
+void SpadsHandler::SetRandomMap()
+{
+    m_battle->Say(_T("!map 1")); //not so random
+}
+
+void SpadsHandler::SetMap(wxString& map)
+{
+    m_battle->Say(_T("!map")+map);
+}
+
+void SpadsHandler::ClearStartBoxes()
+{
+
+}
+
+void SpadsHandler::AddStartBox(int posx,int posy,int w,int h)
+{
+
+}
+
+void SpadsHandler::Notify()
+{
+    m_battle->Say(_T("!notify"));
+}
+
+//-------------
+
+
+AutohostManager::AutohostManager():m_type(AUTOHOSTTYPE_UNKNOWN), m_battle(0)
+{
+
+}
+
+AutohostManager::~AutohostManager()
+{
+
+}
+
+void AutohostManager::SetBattle(Battle* battle)
+{
+    m_battle=battle;
+    m_springie.SetBattle(battle);
+    m_spads.SetBattle(battle);
+}
+
+AutohostHandler& AutohostManager::GetAutohostHandler()
+{
+    switch(m_type)
+    {
+        case AUTOHOSTTYPE_SPRINGIE:
+            return GetSpringie();
+        case AUTOHOSTTYPE_SPADS:
+            return GetSpads();
+    }
+    throw "Unknown autohost";
+}
+
+SpringieHandler& AutohostManager::GetSpringie()
+{
+    return m_springie;
+}
+
+SpadsHandler& AutohostManager::GetSpads()
+{
+    return m_spads;
+}
+
+void AutohostManager::RecnognizeAutohost()
+{
+}
+
+AutohostManager::AutohostType AutohostManager::GetAutohostType()
+{
+    return m_type;
+}
---
+++ src/hosting/autohostmanager.h
@@ -0,0 +1,88 @@
+#ifndef AUTOHOSTMANAGER_H
+#define AUTOHOSTMANAGER_H
+
+#include <wx/string.h>
+#include <exception>
+class Battle;
+
+
+class AutohostHandler
+{
+    public:
+        AutohostHandler();
+        virtual ~AutohostHandler();
+
+        virtual void Balance()=0;
+        virtual void SetRandomMap()=0;
+        virtual void SetMap(wxString& map)=0;
+        virtual void ClearStartBoxes()=0;
+        virtual void AddStartBox(int posx,int posy,int w,int h)=0;
+        virtual void Notify()=0;
+    protected:
+        Battle* m_battle;
+
+    private:
+        void SetBattle(Battle* battle);
+
+
+        friend class AutohostManager;
+};
+
+class SpringieHandler: public AutohostHandler
+{
+    public:
+        SpringieHandler();
+        ~SpringieHandler();
+
+        void Balance();
+        void SetRandomMap();
+        void SetMap(wxString& map);
+        void ClearStartBoxes();
+        void AddStartBox(int posx,int posy,int w,int h);
+        void Notify();
+};
+
+class SpadsHandler: virtual public AutohostHandler
+{
+    public:
+        SpadsHandler();
+        ~SpadsHandler();
+
+        void Balance();
+        void SetRandomMap();
+        void SetMap(wxString& map);
+        void ClearStartBoxes();
+        void AddStartBox(int posx,int posy,int w,int h);
+        void Notify();
+};
+
+class AutohostManager
+{
+    public:
+
+        enum AutohostType
+        {
+            AUTOHOSTTYPE_UNKNOWN,
+            AUTOHOSTTYPE_SPRINGIE,
+            AUTOHOSTTYPE_SPADS
+        };
+
+        AutohostManager();
+        ~AutohostManager();
+
+        void SetBattle(Battle* bt);
+        void RecnognizeAutohost();
+        AutohostType GetAutohostType();
+
+        AutohostHandler& GetAutohostHandler();
+
+        SpringieHandler& GetSpringie();
+        SpadsHandler& GetSpads();
+    private:
+        SpringieHandler m_springie;
+        SpadsHandler m_spads;
+        AutohostType m_type;
+        Battle* m_battle;
+};
+
+#endif // AUTOHOSTMANAGER_H
