3cdc539367c11acc14fac46c0280cf395fc21498
abma <spring@abma.de>
2013-09-18 18:16:31 +0200

    privatize + style

--- src/chatpanel.h
+++ src/chatpanel.h
@@ -67,142 +67,141 @@ struct ChatLine
  */
 class ChatPanel : public wxPanel, public GlobalEvent, public SL::NonCopyable
 {
-  public:
+public:
 
-    ChatPanel( wxWindow* parent, Channel& chan, wxImageList* imaglist );
-    ChatPanel( wxWindow* parent, const User& user, wxImageList* imaglist  );
-    ChatPanel( wxWindow* parent, Server& serv, wxImageList* imaglist  );
-    ChatPanel( wxWindow* parent, Battle* battle );
-    ~ChatPanel();
+	ChatPanel( wxWindow* parent, Channel& chan, wxImageList* imaglist );
+	ChatPanel( wxWindow* parent, const User& user, wxImageList* imaglist  );
+	ChatPanel( wxWindow* parent, Server& serv, wxImageList* imaglist  );
+	ChatPanel( wxWindow* parent, Battle* battle );
+	~ChatPanel();
 
-    void Said( const wxString& who, const wxString& message );
-    void DidAction( const wxString& who, const wxString& action );
-    void Motd( const wxString& message );
-    void StatusMessage( const wxString& message );
-    void ClientMessage( const wxString& message );
+	void Said( const wxString& who, const wxString& message );
+	void DidAction( const wxString& who, const wxString& action );
+	void Motd( const wxString& message );
+	void StatusMessage( const wxString& message );
+	void ClientMessage( const wxString& message );
 
-    void UnknownCommand( const wxString& command, const wxString& params );
+	void UnknownCommand( const wxString& command, const wxString& params );
 
-    void Joined( User& who );
-    void Parted( User& who, const wxString& message );
-    void SetTopic( const wxString& who, const wxString& message );
-    void UserStatusUpdated( User& who );
-    void OnChannelJoin( User& who );
+	void Joined( User& who );
+	void Parted( User& who, const wxString& message );
+	void SetTopic( const wxString& who, const wxString& message );
+	void UserStatusUpdated( User& who );
+	void OnChannelJoin( User& who );
 
-    const Channel* GetChannel() const;
-    void SetChannel( Channel* chan );
+	const Channel* GetChannel() const;
+	void SetChannel( Channel* chan );
 
-    const Server* GetServer()  const;
-    void SetServer( Server* serv );
+	const Server* GetServer()  const;
+	void SetServer( Server* serv );
 
-    const User* GetUser() const ;
-    void SetUser( const User* usr );
+	const User* GetUser() const ;
+	void SetUser( const User* usr );
 
-    bool IsServerPanel() const;
-    ChatPanelType GetPanelType() const;
+	bool IsServerPanel() const;
+	ChatPanelType GetPanelType() const;
 
-    void SetBattle( Battle* battle );
+	void SetBattle( Battle* battle );
 
+	void Part();
+	void FocusInputBox();
+
+
+	size_t GetIconIndex()  const { return m_icon_index; }
+	void SetIconIndex( size_t index ) { m_icon_index = index; }
+
+	void OnUserDisconnected();
+	void OnUserConnected();
+
+	void UpdateNicklistHighlights();
+
+private:
 	//! @returns true on success ( blank line ), false otherwise
 	bool Say( const wxString& message );
-    void Part();
-    void FocusInputBox();
 
-    wxString GetChatTypeStr()  const;
+	wxString GetChatTypeStr()  const;
 
-    size_t GetIconIndex()  const { return m_icon_index; }
-    void SetIconIndex( size_t index ) { m_icon_index = index; }
+	const User& GetMe()  const;
+	const User* GetSelectedUser() const;
 
-    const User& GetMe()  const;
-    const User* GetSelectedUser() const;
+	void SortNickList();
 
-    bool IsOk() const;
+	void OnChanOpts( wxCommandEvent& event );
+	void OnSay( wxCommandEvent& event );
+	void OnPaste( wxClipboardTextEvent& event );
 
-    void OnUserDisconnected();
-    void OnUserConnected();
+	void OnLinkEvent( wxTextUrlEvent& event );
+	void OnMouseDown( wxMouseEvent& event );
 
-    void OnChanOpts( wxCommandEvent& event );
-    void OnSay( wxCommandEvent& event );
-    void OnPaste( wxClipboardTextEvent& event );
+	void OnKeyPressed( wxKeyEvent& keyevent );
+	void OnKeyReleased( wxKeyEvent& keyevent );
 
-    void OnLinkEvent( wxTextUrlEvent& event );
-    void OnMouseDown( wxMouseEvent& event );
-
-    void OnKeyPressed( wxKeyEvent& keyevent );
-    void OnKeyReleased( wxKeyEvent& keyevent );
-
-    void UpdateNicklistHighlights();
-
-    void SortNickList();
-
-    void OnMenuItem( wxCommandEvent& event );
+	bool IsOk() const;
+	void OnMenuItem( wxCommandEvent& event );
 
 	void OnLogin( wxCommandEvent& data );
 
-  protected:
+	void _SetChannel( Channel* channel );
+	void OutputLine( const wxString& message, const wxColour& col, const wxFont& fon );
+	void OutputLine( const ChatLine& line );
+	void SetIconHighlight( HighlightType highlight );
+	wxString FindUrl( const long pos ) const ;
 
-    void _SetChannel( Channel* channel );
-    void OutputLine( const wxString& message, const wxColour& col, const wxFont& fon );
-    void OutputLine( const ChatLine& line );
-    void SetIconHighlight( HighlightType highlight );
-    wxString FindUrl( const long pos ) const ;
+	bool ContainsWordToHighlight( const wxString& message ) const;
+	bool m_show_nick_list;      //!< If the nicklist should be shown or not.
 
-    bool ContainsWordToHighlight( const wxString& message ) const;
+	wxBoxSizer* m_main_sizer;   //!< Main sizer containing all other sizers.
+	wxBoxSizer* m_chat_sizer;   //!< Sizer containing the chat messages, and send input and button.
+	wxBoxSizer* m_say_sizer;    //!< Sizer containing send input and button.
+	wxBoxSizer* m_nick_sizer;   //!< Sizer containing the nicklist.
 
-    bool m_show_nick_list;      //!< If the nicklist should be shown or not.
+	wxSplitterWindow* m_splitter; //!< The splitter.
+	wxPanel* m_chat_panel;      //!< Panel containing the chat. Only used when nicklist is visible.
+	wxPanel* m_nick_panel;      //!< Panel containing the nicklist.
 
-    wxBoxSizer* m_main_sizer;   //!< Main sizer containing all other sizers.
-    wxBoxSizer* m_chat_sizer;   //!< Sizer containing the chat messages, and send input and button.
-    wxBoxSizer* m_say_sizer;    //!< Sizer containing send input and button.
-    wxBoxSizer* m_nick_sizer;   //!< Sizer containing the nicklist.
+	wxTextCtrl* m_chatlog_text; //!< The chat log textcontrol.
+	wxTextCtrlHist* m_say_text;     //!< The say textcontrol.
+	wxBitmapButton* m_chan_opts_button; //!< The channel options button.
 
-    wxSplitterWindow* m_splitter; //!< The splitter.
-    wxPanel* m_chat_panel;      //!< Panel containing the chat. Only used when nicklist is visible.
-    wxPanel* m_nick_panel;      //!< Panel containing the nicklist.
+	NickListCtrl* m_nicklist;   //!< The nicklist.
+	wxComboBox* m_nick_filter;  //!< The filter combo.
 
-    wxTextCtrl* m_chatlog_text; //!< The chat log textcontrol.
-    wxTextCtrlHist* m_say_text;     //!< The say textcontrol.
-    wxBitmapButton* m_chan_opts_button; //!< The channel options button.
+	wxButton* m_say_button;     //!< The say button.
+	SLNotebook* m_chat_tabs;
 
-    NickListCtrl* m_nicklist;   //!< The nicklist.
-    wxComboBox* m_nick_filter;  //!< The filter combo.
+	Channel* m_channel;         //!< Channel object.
+	Server* m_server;           //!< Server object.
+	const User* m_user;               //!< User object.
+	Battle* m_battle;           //!< User object.
 
-    wxButton* m_say_button;     //!< The say button.
-    SLNotebook* m_chat_tabs;
+	wxStaticText* m_usercount_label;
 
-    Channel* m_channel;         //!< Channel object.
-    Server* m_server;           //!< Server object.
-    const User* m_user;               //!< User object.
-    Battle* m_battle;           //!< User object.
+	const ChatPanelType m_type;       //!< Channel object.
 
-    wxStaticText* m_usercount_label;
+	wxString m_chan_pass;
 
-    const ChatPanelType m_type;       //!< Channel object.
+	ChatPanelMenu* m_popup_menu;
 
-    wxString m_chan_pass;
+	ChatLog m_chat_log;
 
-    ChatPanelMenu* m_popup_menu;
+	void LogTime();
+	void CreateControls( );
+	void CreatePopup();
+	void LoadLastLines();
 
-    ChatLog m_chat_log;
+	size_t m_icon_index;
 
-    void LogTime();
-    void CreateControls( );
-    void CreatePopup();
-    void LoadLastLines();
+	wxImageList* m_imagelist;
 
-    size_t m_icon_index;
-
-    wxImageList* m_imagelist;
-
-    static const int m_groupMenu_baseID = 6798;
+	static const int m_groupMenu_baseID = 6798;
 	TextCompletionDatabase textcompletiondatabase;
 
-    std::vector<ChatLine> m_buffer;
-    bool m_disable_append;
+	std::vector<ChatLine> m_buffer;
+	bool m_disable_append;
 
-    wxString m_url_at_pos; //! the mouse event sink sets this
+	wxString m_url_at_pos; //! the mouse event sink sets this
 
-    friend class ChatPanelMenu; //menu needs access to members
+	friend class ChatPanelMenu; //menu needs access to members
 
 	//used to avoid marking channel as changed when it's just been created.
 	bool m_topic_set;
@@ -212,10 +211,10 @@ class ChatPanel : public wxPanel, public GlobalEvent, public SL::NonCopyable
 
 enum
 {
-    CHAT_SEND = wxID_HIGHEST,
-    CHAT_TEXT,
-    CHAT_LOG,
-    CHAT_CHAN_OPTS
+	CHAT_SEND = wxID_HIGHEST,
+	CHAT_TEXT,
+	CHAT_LOG,
+	CHAT_CHAN_OPTS
 };
 #endif // SPRINGLOBBY_HEADERGUARD_CHATPANEL_H
 
