22ace1503e525b8261c6cdc555c2205913c29b55
abma <spring@abma.de>
2013-09-25 21:19:03 +0200

    refactor last commit

---
+++ src/textentrydialog.h
@@ -0,0 +1,34 @@
+#include <wx/string.h>
+#include <wx/dialog.h>
+
+class TextEntryDialog: public wxDialog {
+public:
+	TextEntryDialog(wxWindow* parent, const wxString& title,const wxString& heading, const wxString& answer, bool multiline=false):
+		wxDialog(parent, -1, title, wxDefaultPosition, wxDefaultSize,  wxDEFAULT_DIALOG_STYLE | wxRESIZE_BORDER)
+	{
+		wxStaticText* text = new wxStaticText(this, -1, heading);
+		long inputstyle=0;
+		if (multiline)
+			inputstyle=wxTE_MULTILINE;
+		input = new wxTextCtrl(this, -1, _(""), wxDefaultPosition, wxDefaultSize, inputstyle);
+		if (multiline)
+			input->SetInitialSize(wxSize(400, 300));
+		SetValue(answer);
+		wxSizer* buttons = CreateButtonSizer(wxOK|wxCANCEL);
+		wxBoxSizer* sizer = new wxBoxSizer(wxVERTICAL);
+		sizer->Add(text, 0, wxALL, 5);
+		sizer->Add(input, 1, wxEXPAND|wxALL, 5);
+		sizer->Add(buttons, 0, wxEXPAND|wxALL, 5);
+		SetSizerAndFit(sizer);
+	}
+
+	void SetValue(const wxString& value) {
+		input->SetValue(value);
+	}
+
+	const wxString GetValue() {
+		return input->GetValue();
+	}
+private:
+		wxTextCtrl* input;
+};
--- src/ui.cpp
+++ src/ui.cpp
@@ -56,6 +56,7 @@
 #include "sound/alsound.h"
 #include <lslutils/globalsmanager.h>
 #include "utils/misc.h"
+#include "textentrydialog.h"
 
 static const unsigned int s_reconnect_delay_ms = 6000;
 
@@ -373,38 +374,6 @@ bool Ui::AskPassword( const wxString& heading, const wxString& message, wxString
     password = pw_dlg.GetValue();
     return ( res == wxID_OK );
 }
-
-class TextEntryDialog: public wxDialog {
-public:
-	TextEntryDialog(wxWindow* parent, const wxString& title,const wxString& heading, const wxString& answer, bool multiline=false):
-		wxDialog(parent, -1, title, wxDefaultPosition, wxDefaultSize,  wxDEFAULT_DIALOG_STYLE | wxRESIZE_BORDER)
-	{
-		wxStaticText* text = new wxStaticText(this, -1, heading);
-		long inputstyle=0;
-		if (multiline)
-			inputstyle=wxTE_MULTILINE;
-		input = new wxTextCtrl(this, -1, _(""), wxDefaultPosition, wxDefaultSize, inputstyle);
-		if (multiline)
-			input->SetInitialSize(wxSize(400, 300));
-		SetValue(answer);
-		wxSizer* buttons = CreateButtonSizer(wxOK|wxCANCEL);
-		wxBoxSizer* sizer = new wxBoxSizer(wxVERTICAL);
-		sizer->Add(text, 0, wxALL, 5);
-		sizer->Add(input, 1, wxEXPAND|wxALL, 5);
-		sizer->Add(buttons, 0, wxEXPAND|wxALL, 5);
-		SetSizerAndFit(sizer);
-	}
-
-	void SetValue(const wxString& value) {
-		input->SetValue(value);
-	}
-
-	const wxString GetValue() {
-		return input->GetValue();
-	}
-private:
-		wxTextCtrl* input;
-};
 
 bool Ui::AskText( const wxString& heading, const wxString& question, wxString& answer, bool multiline )
 {
