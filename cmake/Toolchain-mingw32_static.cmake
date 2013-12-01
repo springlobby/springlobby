7a21798c4e92d6706eb9064df969c0e21ec06a7c
abma <spring@abma.de>
2013-12-01 13:30:31 +0100

    try to fix buildbot compile

--- cmake/Toolchain-mingw32_static.cmake
+++ cmake/Toolchain-mingw32_static.cmake
@@ -3,3 +3,6 @@ SET( LOCALE_INSTALL_DIR "${CMAKE_BINARY_DIR}/locale" CACHE STRING
 SET( CMAKE_INSTALL_PREFIX "${CMAKE_BINARY_DIR}" CACHE STRING
 	"install prefix" FORCE )
 
+# FIXME: move to buildbot's master.cfg
+include(/opt/mingw32/usr/i686-w64-mingw32/share/cmake/mxe-conf.cmake)
+
