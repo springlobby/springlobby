e620045e60a08f2ea9286f57b535de9bd3782d69
abma <spring@abma.de>
2014-01-28 17:29:48 +0100

    improve testcases

--- src/tests/config.cpp
+++ src/tests/config.cpp
@@ -7,8 +7,8 @@
 #include <wx/filename.h>
 
 SLCONFIG("/test/string", "test" , "test string");
-SLCONFIG("/test/long", 1l, "test long");
-SLCONFIG("/test/double", 2.0, "test double");
+SLCONFIG("/test/long", -12345l, "test long");
+SLCONFIG("/test/double", -321.123, "test double");
 SLCONFIG("/test/bool", true, "test bool");
 
 namespace SlPaths{
@@ -22,11 +22,11 @@ BOOST_AUTO_TEST_CASE( slconfig )
 {
 
 //	cfg().SaveFile();
-	BOOST_CHECK(cfg().ReadLong(_T("/test/long")) == 1l);
-	BOOST_CHECK(cfg().Read(_T("/test/long")) == _T("1") );
+	BOOST_CHECK(cfg().ReadLong(_T("/test/long")) == -12345l);
+	BOOST_CHECK(cfg().Read(_T("/test/long")) == _T("-12345") );
 
-	BOOST_CHECK(cfg().ReadDouble(_T("/test/double")) == 2.0);
-	BOOST_CHECK(cfg().Read(_T("/test/double")) == _T("2") ); //FIXME: should return 2.0
+	BOOST_CHECK(cfg().ReadDouble(_T("/test/double")) == -321.123);
+	BOOST_CHECK(cfg().Read(_T("/test/double")) == _T("-321.123") );
 
 	BOOST_CHECK(cfg().ReadBool(_T("/test/bool")) == true);
 	BOOST_CHECK(cfg().Read(_T("/test/bool")) == _T("1") );
@@ -34,11 +34,11 @@ BOOST_AUTO_TEST_CASE( slconfig )
 //	BOOST_CHECK(cfg().ReadString(_T("/test/string")) == _T("test")); //FIXME: fails
 	BOOST_CHECK(cfg().Read(_T("/test/string")) == _("") ); //FIXME: should return test
 
-	BOOST_CHECK(cfg().Write(_T("/test/long"), 10l));
-	BOOST_CHECK(cfg().ReadLong(_T("/test/long")) == 10l);
+	BOOST_CHECK(cfg().Write(_T("/test/long"), -10l));
+	BOOST_CHECK(cfg().ReadLong(_T("/test/long")) == -10l);
 
-	BOOST_CHECK(cfg().Write(_T("/test/double"), 10.0));
-	BOOST_CHECK(cfg().ReadDouble(_T("/test/double")) == 10.0);
+	BOOST_CHECK(cfg().Write(_T("/test/double"), 10.3));
+	BOOST_CHECK(cfg().ReadDouble(_T("/test/double")) == 10.3);
 
 	BOOST_CHECK(cfg().Write(_T("/test/bool"), false));
 	BOOST_CHECK(cfg().ReadBool(_T("/test/bool")) == false);
