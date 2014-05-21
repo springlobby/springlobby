5836bdb64a83e85d85f184e60b8df9939ad089fc
abma <spring@abma.de>
2014-05-22 01:30:34 +0200

    try to fix release build

--- tools/make_dist.sh
+++ tools/make_dist.sh
@@ -5,6 +5,10 @@ cd $(dirname $0)/..
 TARFILE=${1}/${2}.tar
 rm -f ${TARFILE}
 pwd
+
+#FIXME: hack for getting VERSION file into archive
+echo ${3} > ${1}/VERSION
+
 ./tools/git-archive-all --prefix ${2}/  ${TARFILE}
 tar -Prf ${TARFILE} ${1}/springlobby_config.h ${1}/VERSION \
 	--transform "s;${1}/springlobby_config.h;${2}/springlobby_config.h;g" \
