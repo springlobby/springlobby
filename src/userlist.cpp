076d823197256567ee6a9f0c772c486bb924a4e9
abma <spring@abma.de>
2015-03-10 10:01:15 +0100

    fix compile warning:
    suggest explicit braces to avoid ambiguous 'else'

--- src/userlist.cpp
+++ src/userlist.cpp
@@ -55,12 +55,13 @@ void UserList::RemoveUser( const std::string& nick )
 
 User& UserList::GetUser( const std::string& nick ) const
 {
-  user_const_iter_t u = m_users.find(nick);
+	user_const_iter_t u = m_users.find(nick);
   //ASSERT_EXCEPTION( u != m_users.end(), _T("UserList::GetUser(\"") + TowxString(nick) + _T("\"): no such user") );
-  if( u == m_users.end() )
-    wxLogWarning(_T("UserList::GetUser(\"") + TowxString(nick) + _T("\"): no such user"));
+	if( u == m_users.end() ) {
+		wxLogWarning(_T("UserList::GetUser(\"") + TowxString(nick) + _T("\"): no such user"));
+	}
   //ASSERT_LOGIC( u != m_users.end(), _T("UserList::GetUser(\"") + nick + _T("\"): no such user") );
-  return *u->second;
+	return *u->second;
 }
 
 User& UserList::GetUser( user_map_t::size_type index ) const
