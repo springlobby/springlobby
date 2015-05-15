9f712b73b090ee4fb8791ab0b44ea0bdd9d1b13b
abma <spring@abma.de>
2015-05-16 01:48:34 +0200

    candidate for #435

--- src/socket.cpp
+++ src/socket.cpp
@@ -214,13 +214,11 @@ void Socket::SetTimeout(const int seconds)
 //! @brief Disconnect from remote host if connected.
 void Socket::Disconnect()
 {
-	if (m_sock.IsOk()) {
-		const bool wasconnected = m_sock.IsConnected();
-		m_buffer.clear();
-		m_sock.Close();
-		if (wasconnected) { //.Close() disables all events, fire it manually (as last to prevent recursions loops)
-			m_net_class.OnDisconnected();
-		}
+	const bool wasconnected = m_sock.IsConnected();
+	m_buffer.clear();
+	m_sock.Close();
+	if (wasconnected) { //.Close() disables all events, fire it manually (as last to prevent recursions loops)
+		m_net_class.OnDisconnected();
 	}
 }
 
