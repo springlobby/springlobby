2c2ac8eafe4fa77b15d263dcf25f1bdf9b38781f
abma <spring@abma.de>
2015-05-23 14:12:28 +0200

    move code into .cpp

---
+++ src/gui/textentrydialog.cpp
@@ -0,0 +1,42 @@
+/* This file is part of the Springlobby (GPL v2 or later), see COPYING */
+
+#include "textentrydialog.h"
+#include <wx/stattext.h>
+#include <wx/sizer.h>
+#include <wx/textctrl.h>
+
+TextEntryDialog::TextEntryDialog(wxWindow* parent, const wxString& title, const wxString& heading, const wxString& answer, bool multiline)
+	    : wxDialog(parent, -1, title, wxDefaultPosition, wxDefaultSize, wxDEFAULT_DIALOG_STYLE | wxRESIZE_BORDER)
+{
+	wxStaticText* text = new wxStaticText(this, -1, heading);
+	long inputstyle = 0;
+	if (multiline)
+		inputstyle = wxTE_MULTILINE;
+	input = new wxTextCtrl(this, -1, wxEmptyString, wxDefaultPosition, wxDefaultSize, inputstyle);
+	if (multiline)
+		input->SetInitialSize(wxSize(400, 300));
+	SetValue(answer);
+	wxSizer* buttons = CreateButtonSizer(wxOK | wxCANCEL);
+	wxBoxSizer* sizer = new wxBoxSizer(wxVERTICAL);
+	sizer->Add(text, 0, wxALL, 5);
+	sizer->Add(input, 1, wxEXPAND | wxALL, 5);
+	sizer->Add(buttons, 0, wxEXPAND | wxALL, 5);
+	SetSizerAndFit(sizer);
+	input->SetFocus();
+}
+
+TextEntryDialog::~TextEntryDialog()
+{
+}
+
+void TextEntryDialog::SetValue(const wxString& value)
+{
+	input->SetValue(value);
+}
+
+const wxString TextEntryDialog::GetValue()
+{
+	return input->GetValue();
+}
+
+
--- src/gui/textentrydialog.h
+++ src/gui/textentrydialog.h
@@ -2,46 +2,16 @@
 
 #include <wx/string.h>
 #include <wx/dialog.h>
-#include <wx/stattext.h>
-#include <wx/sizer.h>
+
+class wxTextCtrl;
 
 class TextEntryDialog : public wxDialog
 {
 public:
-	TextEntryDialog(wxWindow* parent, const wxString& title, const wxString& heading, const wxString& answer, bool multiline = false)
-	    : wxDialog(parent, -1, title, wxDefaultPosition, wxDefaultSize, wxDEFAULT_DIALOG_STYLE | wxRESIZE_BORDER)
-	{
-		wxStaticText* text = new wxStaticText(this, -1, heading);
-		long inputstyle = 0;
-		if (multiline)
-			inputstyle = wxTE_MULTILINE;
-		input = new wxTextCtrl(this, -1, wxEmptyString, wxDefaultPosition, wxDefaultSize, inputstyle);
-		if (multiline)
-			input->SetInitialSize(wxSize(400, 300));
-		SetValue(answer);
-		wxSizer* buttons = CreateButtonSizer(wxOK | wxCANCEL);
-		wxBoxSizer* sizer = new wxBoxSizer(wxVERTICAL);
-		sizer->Add(text, 0, wxALL, 5);
-		sizer->Add(input, 1, wxEXPAND | wxALL, 5);
-		sizer->Add(buttons, 0, wxEXPAND | wxALL, 5);
-		SetSizerAndFit(sizer);
-		input->SetFocus();
-	}
-
-	virtual ~TextEntryDialog()
-	{
-	}
-
-	void SetValue(const wxString& value)
-	{
-		input->SetValue(value);
-	}
-
-	const wxString GetValue()
-	{
-		return input->GetValue();
-	}
-
+	TextEntryDialog(wxWindow* parent, const wxString& title, const wxString& heading, const wxString& answer, bool multiline = false);
+	virtual ~TextEntryDialog();
+	void SetValue(const wxString& value);
+	const wxString GetValue();
 private:
 	wxTextCtrl* input;
 };
