9fbf48dc7c01f48bc38cebb16f5e8712c12168d5
abma <spring@abma.de>
2015-10-25 22:00:14 +0100

    Revert "fix LoadPopular():"
    This reverts commit 6507e7ff846ff9dd6bae04df7c426461aca944f0.

--- src/gui/mapselectdialog.cpp
+++ src/gui/mapselectdialog.cpp
@@ -352,11 +352,8 @@ void MapSelectDialog::LoadPopular()
 			IBattle* b = serverSelector().GetServer().battles_iter->GetBattle();
 			const std::string mapname = b->GetHostMapName();
 			assert(!mapname.empty());
-			if (b == NULL)
-				continue;
-			if (!LSL::usync().MapExists(mapname))
-				continue;
-			m_mapgrid->AddMap(TowxString(mapname));
+			if (b != NULL)
+				m_mapgrid->AddMap(TowxString(mapname));
 		}
 	} catch (...) {
 	} // ui().GetServer may throw when disconnected...
