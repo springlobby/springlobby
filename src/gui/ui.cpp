2b7d832d9dbacdeaac6a372e9a9b99c0c4b31913
abma <spring@abma.de>
2015-11-04 00:15:25 +0100

    update prd + fix one mingw compile error

--- src/gui/ui.cpp
+++ src/gui/ui.cpp
@@ -943,12 +943,7 @@ void Ui::CheckForUpdates(bool show)
 					      _("Not up to date"), wxOK | wxCANCEL);
 		if (answer == wxOK) {
 			try{
-				if (!contMan->UpdateApplication()) {
-					//this will also happen if updater exe is not present so we don't really ne special check for existance of it
-					customMessageBox(SL_MAIN_ICON, _("Automatic update failed\n\nyou will be redirected to a web page with instructions and the download link will be opened in your browser.") + msg, _("Updater error."));
-					OpenWebBrowser(_T("https://github.com/springlobby/springlobby/wiki/Install#Windows_Binary"));
-					OpenWebBrowser(TowxString(GetDownloadUrl(STD_STRING(latestVersion))));
-				}
+				contMan->UpdateApplication();
 			} catch(Exception& ex) {
 				//this will also happen if updater exe is not present so we don't really ne special check for existance of it
 				const wxString errormsg = wxString::Format(_("Automatic update failed\n\
