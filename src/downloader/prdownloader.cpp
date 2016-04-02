8504a0992ec485efdc6fe00664e314d07f1dff81
Usaga <masturofpuppets@gmail.com>
2016-04-02 19:40:06 +0600

    Added even more debug logging

--- src/downloader/prdownloader.cpp
+++ src/downloader/prdownloader.cpp
@@ -47,6 +47,7 @@ public:
 	void Run()
 	{
 		slLogDebugFunc("");
+		wxLogDebug("Starting download of filename: %s, name: %s", m_filename.c_str(), m_name.c_str());
 
 		{
 			boost::mutex::scoped_lock lock(dlProgressMutex);
@@ -57,17 +58,22 @@ public:
 		}
 
 		const bool force = true;
+		slLogDebugFunc("");
 		DownloadSetConfig(CONFIG_RAPID_FORCEUPDATE, &force);
 		int results = 0;
+
 		switch(m_category) {
 			case DownloadEnum::CAT_SPRINGLOBBY:
 			case DownloadEnum::CAT_HTTP:
+				slLogDebugFunc("");
 				results = DownloadAddByUrl(m_category, m_filename.c_str(), m_name.c_str());
 				break;
 			default:
+				slLogDebugFunc("");
 				results = DownloadSearch(m_category, m_name.c_str());
 		}
 		if (results <= 0) {
+			slLogDebugFunc("");
 			GlobalEventManager::Instance()->Send(GlobalEventManager::OnDownloadFailed);
 			wxLogInfo("Nothing found to download");
 			return;
@@ -78,20 +84,26 @@ public:
 		}
 
 		downloadInfo info;
+		slLogDebugFunc("");
 		const bool hasdlinfo = DownloadGetInfo(0, info);
 		//In case if something gone wrong
 		if (!hasdlinfo) {
+			slLogDebugFunc("");
 			GlobalEventManager::Instance()->Send(GlobalEventManager::OnDownloadFailed);
 			return;
 		}
 
+		slLogDebugFunc("");
 		GlobalEventManager::Instance()->Send(GlobalEventManager::OnDownloadStarted);
 
+		slLogDebugFunc("");
 		const bool downloadFailed = DownloadStart();
 
 		if (downloadFailed) {
+			slLogDebugFunc("");
 			GlobalEventManager::Instance()->Send(GlobalEventManager::OnDownloadFailed);
 		} else {
+			slLogDebugFunc("");
 			DownloadFinished(m_category, info);
 			GlobalEventManager::Instance()->Send(GlobalEventManager::OnDownloadComplete);
 		}
