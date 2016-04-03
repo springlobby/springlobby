ff1ee0fb7eab93d226cba8b739b92eda7a2cb0be
Usaga <masturofpuppets@gmail.com>
2016-04-03 09:44:55 +0600

    DownloadDataViewCtrl disabled from receiving events

--- src/gui/downloaddataviewctrl.cpp
+++ src/gui/downloaddataviewctrl.cpp
@@ -35,22 +35,22 @@ DownloadDataViewCtrl::DownloadDataViewCtrl(const wxString dataViewName,
 	LoadColumnProperties();
 
 //FIXME!!!!
-//#ifndef WIN32
+#ifndef WIN32
 	GlobalEventManager::Instance()->Subscribe(this,
 			GlobalEventManager::OnDownloadStarted,
 			wxObjectEventFunction(&DownloadDataViewCtrl::OnDownloadStarted));
 	GlobalEventManager::Instance()->Subscribe(this,
 			GlobalEventManager::OnDownloadProgress,
 			wxObjectEventFunction(&DownloadDataViewCtrl::OnDownloadProgress));
-//#endif
+#endif
 }
 
 DownloadDataViewCtrl::~DownloadDataViewCtrl()
 {
 //FIXME!!!!
-//#ifndef WIN32
+#ifndef WIN32
 	GlobalEventManager::Instance()->UnSubscribeAll(this);
-//#endif
+#endif
 	Clear();
 }
 
