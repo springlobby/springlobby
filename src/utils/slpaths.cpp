a0b2ca06beb4a12bf5b0cbbeb07f8cea40264f52
abma <spring@abma.de>
2016-07-21 01:34:30 +0200

    be more harsh about setting SpringData

--- src/utils/slpaths.cpp
+++ src/utils/slpaths.cpp
@@ -459,7 +459,7 @@ std::string SlPaths::GetUpdateDir()
 void SlPaths::SetDownloadDir(const std::string& newDir)
 {
 	std::string newDownloadDir = LSL::Util::EnsureDelimiter(newDir);
-
+	LSL::usync().SetSpringDataPath(newDownloadDir);
 	cfg().Write(_T("/Spring/DownloadDir"), TowxString(newDownloadDir));
 }
 
