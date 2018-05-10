ec48f70e2ae8b1b6f723a4ffecf0d5203cd71954
abma <spring@abma.de>
2018-05-10 22:14:00 +0200

    http -> https

--- src/gui/contentdownloaddialog.cpp
+++ src/gui/contentdownloaddialog.cpp
@@ -85,7 +85,7 @@ void ContentDownloadDialog::Search(const wxString& str)
 {
 	//FIXME: make async!
 	const std::string param = ConvToIRI(STD_STRING(str));
-	const std::string query = stdprintf("http://api.springfiles.com/json.php?nosensitive=on&logical=or&springname=%s&tag=%s", param.c_str(), param.c_str());
+	const std::string query = stdprintf("https://api.springfiles.com/json.php?nosensitive=on&logical=or&springname=%s&tag=%s", param.c_str(), param.c_str());
 	std::string res;
 	CHttpDownloader::DownloadUrl(query, res);
 	CHttpDownloader::ParseResult(STD_STRING(str), res, dls);
