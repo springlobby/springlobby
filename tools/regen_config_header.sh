8f9c1872947dd4ba7421fea075bf38cb1eee3797
Fedja Beader <fedja@protonmail.ch>
2018-06-19 15:22:54 +0200

    Fix compile when any path includes whitespace

--- tools/regen_config_header.sh
+++ tools/regen_config_header.sh
@@ -8,8 +8,8 @@ fi
 
 set -u
 
-SOURCE="${1}"
-OUTPUT="${2}"
+SOURCE="$1"
+OUTPUT="$2"
 
 REV="$(git -C "$SOURCE" describe --tags)" 2>/dev/null
 
@@ -18,20 +18,19 @@ if [ -z "$REV" ]; then
 fi
 
 OLDREV=""
-if [ -s ${SOURCE}/VERSION ] && [ -s ${SOURCE}/springlobby_config.h ]; then
-	OLDREV=$(cat ${SOURCE}/VERSION)
+if [ -s "$SOURCE/VERSION" ] && [ -s "$SOURCE/springlobby_config.h" ]; then
+	read OLDREV < "$SOURCE/VERSION"
 fi
 
-if [ "${OLDREV}" != "${REV}" ]; then # version changed, update file
-	if [ "${REV}" = "unknown" ]; then # new version couldn't be determinated
-		if [ -n "${OLDREV}" ]; then # don't set as old rev is known
-			echo "Couldn't determinate version, using old rev ${OLDREV}"
+if [ "$OLDREV" != "$REV" ]; then # version changed, update file
+	if [ "$REV" = "unknown" ]; then # new version could not be determined
+		if [ -n "$OLDREV" ]; then # don't set as old rev is known
+			echo "Could not determine version, using old rev $OLDREV"
 			exit
 		fi
 	fi
 
-	echo "Updating from version ${OLDREV} to ${REV}"
-	echo -n ${REV}>${OUTPUT}/VERSION
-	sed "s;@SPRINGLOBBY_REV@;${REV};g" ${SOURCE}/cmake/config.h > ${OUTPUT}/springlobby_config.h
+	echo "Updating from version $OLDREV to $REV"
+	echo -n "$REV" > "$OUTPUT/VERSION"
+	sed "s;@SPRINGLOBBY_REV@;${REV};g" "$SOURCE/cmake/config.h" > "$OUTPUT/springlobby_config.h"
 fi
-
