c078da3e26bfd21cad0a3fa2c2ed309aa5964e2a
abma <spring@abma.de>
2019-11-24 14:25:35 +0100

    cleanup / partly rework upload script

---
+++ tools/upload-build.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+
+import subprocess, os, shutil
+
+print(os.getcwd())
+
+
+strip = "/opt/mingw32/usr/bin/i686-w64-mingw32-strip"
+
+with open("VERSION") as f:
+	version = f.read().strip()
+assert(len(version) > 0)
+
+if "-" is in version:
+	outdir = "develop"
+	verfile = "version-develop.txt"
+else:
+	outdir = "stable"
+	verfile = "version.txt"
+
+verfile = os.path.join(outdir, verfile)
+
+with open(verfile, "w") as f:
+	f.write(version)
+
+shutil.move("src/springlobby.exe", outdir)
+shutil.move("src/springlobby.exe", outdir)
+
+subprocess.checkoutput([strip, "springlobby.exe"])
+subprocess.checkoutput([strip, "springlobby_updater.exe"])
+
+print("Rsync...")
+subprocess.check_output(["rsync", "-av", installdir, "springlobby@springrts.com:/home/springlobby/www/" ], timeout = 600)
+
+"""
+zip -9 -u ${filename} springlobby.exe springlobby_updater.exe
+if [ x$3 == x ]; then
+	filename=sl_master.zip
+else
+	filename=sl_${3}.zip
+fi
+"""
+
