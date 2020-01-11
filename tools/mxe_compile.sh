10a26c4fab77d67aae981c619e9d267cdb7b5c56
abma <spring@abma.de>
2020-01-11 22:43:26 +0100

    try gcc8

--- tools/mxe_compile.sh
+++ tools/mxe_compile.sh
@@ -3,6 +3,6 @@
 set -e
 
 cd mxe
-make MXE_USE_CCACHE= MXE_PLUGIN_DIRS=plugins/gcc9
+make MXE_USE_CCACHE= MXE_PLUGIN_DIRS=plugins/gcc8
 
 echo "done! now use mxe/usr/bin/i686-w64-mingw32.static-cmake to configure!"
