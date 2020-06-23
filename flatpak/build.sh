aa314b85cd4f4c86b6c909a4a0df32ea2972ec2b
abma <spring@abma.de>
2020-06-23 23:28:51 +0200

    flatpak: add alure

--- flatpak/build.sh
+++ flatpak/build.sh
@@ -1,10 +1,8 @@
 FLATHUB=https://flathub.org/repo/flathub.flatpakrepo
 
-dnf upgrade -y
-dnf install -y flatpak-builder git wget
 flatpak remote-add --if-not-exists flathub $FLATHUB
 flatpak install -y flathub org.freedesktop.Platform 18.08
 flatpak install -y flathub org.freedesktop.Sdk 18.08
 flatpak-builder --repo=repo ./build com.springrts.SpringLobby.json
-flatpak build-bundle repo wxformbuilder.flatpak com.springrts.SpringLobby --runtime-repo=$FLATHUB
+flatpak build-bundle repo springlobby.flatpak com.springrts.SpringLobby --runtime-repo=$FLATHUB
 
