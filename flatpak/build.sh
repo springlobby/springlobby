eb07cb6c2639d3591ff3ba9abcda1ef9017eb9ec
abma <spring@abma.de>
2020-06-24 13:16:40 +0200

    remove flatpak: -> https://github.com/baracoder/com.springrts.springlobby
    part of #964

--- flatpak/build.sh
+++
@@ -1,8 +0,0 @@
-FLATHUB=https://flathub.org/repo/flathub.flatpakrepo
-
-flatpak remote-add --if-not-exists flathub $FLATHUB
-flatpak install -y flathub org.freedesktop.Platform 18.08
-flatpak install -y flathub org.freedesktop.Sdk 18.08
-flatpak-builder --repo=repo ./build com.springrts.SpringLobby.json
-flatpak build-bundle repo springlobby.flatpak com.springrts.SpringLobby --runtime-repo=$FLATHUB
-
